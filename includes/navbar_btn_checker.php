771c03102aba30e8691b112315be35c778bb793e
kmarcell0 <kmarcell.kovi@gmail.com>
2020-10-05 22:28:02 +0200

    navbar_btn_checker.php hot fix

--- includes/navbar_btn_checker.php
+++ includes/navbar_btn_checker.php
@@ -87,7 +87,7 @@
         break;
         case 2: /* published */
 
-            $array_button_names_for_change = ["general", "basic_info", "info_for_fencers", "timetable", "ranking"];
+            $array_button_names_for_change = ["basic_info", "info_for_fencers", "timetable", "ranking"];
 
             foreach ($array_button_names_for_change as $name) {
                 $assoc_array_functions[$name] = DISABLED_CLASS;
