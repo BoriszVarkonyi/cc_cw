4a14ee1f198fd80d2e54dd1cffeef48afbbfb057
WolframKristof <wolfram.kristof@gmail.com>
2020-10-14 22:28:31 +0200

    kód takarítás

--- php/invitation.php
+++ php/invitation.php
@@ -14,7 +14,7 @@ $getdata_do = mysqli_query($connection, $qry_getdata);
 
     if($row = mysqli_fetch_assoc($getdata_do)) {
 
-        //putting the data from database into an assoc array with $array_getdata[] indexes
+        //Putting the data from database into an assoc array with $array_getdata[] indexes
         for ($i = 0; $i <  count($array_getdata); ++$i) {
 
             if ($i == 0) {
@@ -47,170 +47,173 @@ $getdata_do = mysqli_query($connection, $qry_getdata);
         <?php include "../includes/navbar.php"; ?>
         <!-- navbar -->
         <div class="page_content_flex">
-                <div id="title_stripe">
-                    <p class="page_title">Plus Information</p>
-                    <button class="stripe_button" type="button" onclick="printPage()">
-                        <p>Print Invitation</p>
-                        <img src="../assets/icons/print-black-18dp.svg"></img>
-                    </button>
-                    <button class="stripe_button orange" type="button">
-                        <p>Save Invitation</p>
-                        <img src="../assets/icons/save-black-18dp.svg"></img>
-                    </button>
-                </div>
+            <div id="title_stripe">
+                <p class="page_title">Plus Information</p>
+                <button class="stripe_button" type="button" onclick="printPage()">
+                    <p>Print Invitation</p>
+                    <img src="../assets/icons/print-black-18dp.svg"></img>
+                </button>
+                <button class="stripe_button orange" type="button">
+                    <p>Save Invitation</p>
+                    <img src="../assets/icons/save-black-18dp.svg"></img>
+                </button>
+            </div>
 
-                <p><?php echo $feedback ?></p>
+            <p><?php echo $feedback ?></p>
 
-                <div id="page_content_panel_main">
-                    <div id="invitation_wrapper" class="wrapper">
+            <div id="page_content_panel_main">
+                <div id="invitation_wrapper" class="wrapper">
 
-                        <div id="plus_information">
-                            <div class="db_panel_title_stripe">
-                                <img src="../assets/icons/build-black-18dp.svg" alt="" class="db_panel_stripe_icon">
-                                <p>Plus information</p>
-                            </div>
-                            <div class="db_panel_main">
-                                <div id="plus_info_wrapper">
+                    <div id="plus_information">
+                        <div class="db_panel_title_stripe">
+                            <img src="../assets/icons/build-black-18dp.svg" alt="" class="db_panel_stripe_icon">
+                            <p>Plus information</p>
+                        </div>
+                        <div class="db_panel_main">
+                            <div id="plus_info_wrapper">
 
-                                    <div class="entry" id="">
-                                        <div class="table_row" onclick="toggleEntry(this)">
-                                            <div class="table_item invitation">Hungarian Fencing Federation</div>
-                                        </div>
-                                        <div class="entry_panel collapsed">
-                                            <textarea name="" id=""></textarea>
-                                            <input type="text" class="hidden">
-                                            <button class="panel_submit">Save</button>
+                                <div class="entry" id="">
+                                    <div class="table_row" onclick="toggleEntry(this)">
+                                        <div class="table_item invitation">Hungarian Fencing Federation</div>
+                                    </div>
+                                    <div class="entry_panel collapsed">
+                                        <textarea name="" id=""></textarea>
+                                        <input type="text" class="hidden">
+                                        <button class="panel_submit">Save</button>
+                                    </div>
+                                </div>
+
+                                <div id="add_entry" onclick="addEntry()">
+                                    <div class="table_row" onclick="">
+                                        <div class="table_item">
+                                            Add information
+                                            <img src="../assets/icons/add-black-18dp.svg" alt="">
                                         </div>
                                     </div>
+                                </div>
 
-                                    <div id="add_entry" onclick="addEntry()">
-                                        <div class="table_row" onclick="">
-                                            <div class="table_item">
-                                                Add information
-                                                <img src="../assets/icons/add-black-18dp.svg" alt="">
-                                            </div>
+                                <form id="adding_entry">
+                                    <div class="table_row" onclick="">
+                                        <div class="table_item">
+                                            <input type="text" placeholder="Type in the title">
+                                            <button class="save_entry">Create</button>
                                         </div>
                                     </div>
+                                </form>
 
-                                    <form id="adding_entry">
-                                        <div class="table_row" onclick="">
-                                            <div class="table_item">
-                                                <input type="text" placeholder="Type in the title">
-                                                <button class="save_entry">Create</button>
-                                            </div>
-                                        </div>
-                                    </form>
-
-                                    <div class="entry" id="">
-                                        <div class="table_row" onclick="toggleEntry(this)">
-                                            <div class="table_item invitation">Hungarian Fencing Federation</div>
-                                        </div>
-                                        <div class="entry_panel collapsed">
-                                            <textarea name="" id=""></textarea>
-                                            <input type="text" class="hidden">
-                                            <button class="panel_submit">Save</button>
-                                        </div>
+                                <div class="entry" id="">
+                                    <div class="table_row" onclick="toggleEntry(this)">
+                                        <div class="table_item invitation">Hungarian Fencing Federation</div>
+                                    </div>
+                                    <div class="entry_panel collapsed">
+                                        <textarea name="" id=""></textarea>
+                                        <input type="text" class="hidden">
+                                        <button class="panel_submit">Save</button>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="inv_panel" id="">
-                            <div class="db_panel_title_stripe">
-                                <img src="../assets/icons/build-black-18dp.svg" alt="" class="db_panel_stripe_icon">
-                                <p>Add competition logo</p>
-                            </div>
-                            <div class="db_panel_main">
-                                <!--Only visible when file is uploaded-->
-                                <button class="panel_button">
-                                    <img src="../assets/icons/delete-black-18dp.svg" alt="">
-                                </button>
-                                <div class="invitation_file_wrapper">  
-                                    <input type="file" id="header_img">
-                                    <label for="header_img">Upload picture</label> <!--Has to rewritten to file's name after uploading-->
-                                </div>
-                            </div>
+                    </div>
+                    <div class="inv_panel" id="">
+                        <div class="db_panel_title_stripe">
+                            <img src="../assets/icons/build-black-18dp.svg" alt="" class="db_panel_stripe_icon">
+                            <p>Add competition logo</p>
                         </div>
-
-                        <div id="cw_preview">
-                            <div id="comp_data">
+                        <div class="db_panel_main">
+                            <!--Only visible when file is uploaded-->
+                            <button class="panel_button">
                                 <img src="../assets/icons/delete-black-18dp.svg" alt="">
-
-                                <p class="cw_panel_title"><?php echo $comp_name ?></p>
-                                <p id="comp_status">Ongoing</p>
-
-                                <div>
-                                    <p><?php echo sexConverter($assoc_array_data['comp_name']) . "'s" ?></p>
-                                    <p><?php echo weaponConverter($assoc_array_data['comp_weapon']) ?></p>
-                                    <p><?php echo date('Y', strtotime($assoc_array_data['comp_start'])) ?></p>
-                                </div>
+                            </button>
+                            <div class="invitation_file_wrapper">  
+                                <input type="file" id="header_img">
+                                <label for="header_img">Upload picture</label> <!--Has to rewritten to file's name after uploading-->
                             </div>
+                        </div>
+                    </div>
 
-                                <!-- basic info panel -->
-                                <div id="basic_information_panel">
-                                    <div>
-                                        <p class="data_label">HOST COUNTRY:</p>
-                                        <p><?php echo $assoc_array_data['comp_host'] ?></p>
-                                        <p class="data_label">LOCATION AND ADDRESS:</p>
-                                        <p><?php echo $assoc_array_data['comp_location'] ?></p>
-                                        <p class="data_label">ENTRY-FEE:</p>
-                                        <p><?php echo $assoc_array_data['comp_entry'] . " Ft"; ?></p>
-                                    </div>
-                                    <div>
-                                        <p class="data_label">STARTING DATE:</p>
-                                        <p><?php echo $assoc_array_data['comp_start'] ?></p>
-                                        <p class="data_label">ENDING DATE:</p>
-                                        <p><?php echo $assoc_array_data['comp_end'] ?></p>
-                                        <p class="data_label pre_reg">END OF PRE-REGISTRTATION:</p>
-                                        <p><?php echo $assoc_array_data['comp_pre_end'] ?></p>
-                                    </div>
-                                </div>
+                    <div id="cw_preview">
 
-                                <!-- equipment panel -->
-                                <div id="equipment_panel">
-                                    <p class="data_label panel_title">EQUIPMENT NEEDED TO BE CHECKED</p>
+                        <div id="comp_data">
+                            <img src="../assets/icons/delete-black-18dp.svg" alt="">
 
-                                    <!-- weapons check table rows -->
-                                    <div>
-                                        <?php 
-                                            $equipment = array("Epee","Foil","Sabre","Electric Jacket","Plastron","Under-Plastron","Socks","Mask","Gloves","Bodywire","Maskwire","Chest protector","Metallic glove");
+                            <p class="cw_panel_title"><?php echo $comp_name ?></p>
+                            <p id="comp_status">Ongoing</p>
 
-                                            $array_equipment = explode(",", $assoc_array_data['comp_equipment']);
+                            <div>
+                                <p><?php echo sexConverter($assoc_array_data['comp_name']) . "'s" ?></p>
+                                <p><?php echo weaponConverter($assoc_array_data['comp_weapon']) ?></p>
+                                <p><?php echo date('Y', strtotime($assoc_array_data['comp_start'])) ?></p>
+                            </div>
+                        </div>
 
-                                            for ($i = 0; $i < count($equipment); $i++) {
-                                                
-                                                if ($array_equipment[$i] != 0) {
-                                                    ?>
-                                                        <div class="table_row">
-                                                            <div class="table_item"><?php echo $equipment[$i] ?></div>
-                                                            <div class="table_item"><?php echo $array_equipment[$i] ?></div>
-                                                    
-                                                        </div>
-                                                    <?php
-                                                }
-                                            }
+                            <!-- basic info panel -->
+                        <div id="basic_information_panel">
+                            <div>
+                                <p class="data_label">HOST COUNTRY:</p>
+                                <p><?php echo $assoc_array_data['comp_host'] ?></p>
+                                <p class="data_label">LOCATION AND ADDRESS:</p>
+                                <p><?php echo $assoc_array_data['comp_location'] ?></p>
+                                <p class="data_label">ENTRY-FEE:</p>
+                                <p><?php echo $assoc_array_data['comp_entry'] . " Ft"; ?></p>
+                            </div>
+                            <div>
+                                <p class="data_label">STARTING DATE:</p>
+                                <p><?php echo $assoc_array_data['comp_start'] ?></p>
+                                <p class="data_label">ENDING DATE:</p>
+                                <p><?php echo $assoc_array_data['comp_end'] ?></p>
+                                <p class="data_label pre_reg">END OF PRE-REGISTRTATION:</p>
+                                <p><?php echo $assoc_array_data['comp_pre_end'] ?></p>
+                            </div>
+                        </div>
+
+                        <!-- equipment panel -->
+                        <div id="equipment_panel">
+                            <p class="data_label panel_title">EQUIPMENT NEEDED TO BE CHECKED</p>
+
+                            <!-- weapons check table rows -->
+                            <div>
+                                <?php 
+                                    $equipment = array("Epee","Foil","Sabre","Electric Jacket","Plastron","Under-Plastron","Socks","Mask","Gloves","Bodywire","Maskwire","Chest protector","Metallic glove");
+
+                                    $array_equipment = explode(",", $assoc_array_data['comp_equipment']);
+
+                                    for ($i = 0; $i < count($equipment); $i++) {
+
+                                     if ($array_equipment[$i] != 0) {
                                         ?>
-                                    </div>
+                                            <div class="table_row">
+                                                <div class="table_item"><?php echo $equipment[$i] ?></div>
+                                                <div class="table_item"><?php echo $array_equipment[$i] ?></div>
+                                            </div>
+                                        <?php
+                                            }
+                                        }
+                                    ?>
                                 </div>
+                            </div>
 
-                                <!-- additional info panel -->
-                                <div id="additional_panel">
-                                    <p class="data_label panel_title">ADDITIONAL INFORMATION FOR FENCERS</p>
-                                    <div>
-                                        <p><?php echo $assoc_array_data['comp_info'] ?></p>
-                                    </div>
+                            <!-- additional info panel -->
+                            <div id="additional_panel">
+                                <p class="data_label panel_title">ADDITIONAL INFORMATION FOR FENCERS</p>
+                                <div>
+                                    <p><?php echo $assoc_array_data['comp_info'] ?></p>
                                 </div>
+                            </div>
 
-                                <!-- weapon control panel -->
-                                <div id="weapon_control_panel">
-                                    <p class="data_label panel_title">WEAPON CONTROL</p>
-                                    <div>
-                                        <div class="weapon_control_day">
-                                            <p>{Weapon Control Date}</p>
-                                            <a>Book appointment</a>
-                                        </div>
+                            <!-- weapon control panel -->
+                            <div id="weapon_control_panel">
+                                <p class="data_label panel_title">WEAPON CONTROL</p>
+                                <div>
+                                    <div class="weapon_control_day">
+                                        <p>{Weapon Control Date}</p>
+                                        <a>Book appointment</a>
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </body>
 <script src="../js/main.js"></script>
