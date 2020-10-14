870f4f72dbcdf797e04b59add5ec747b85addd8f
kmarcell0 <kmarcell.kovi@gmail.com>
2020-10-14 21:06:21 +0200

    php/index.php contains now include/status_comp_table.php and status_comp.php and is now harder better faster stronger, publish competition button is now disabled

--- php/index.php
+++ php/index.php
@@ -12,8 +12,120 @@ checkComp($connection);
     $comp_name = $row["comp_name"];
     
     }
+
+
+    
+    //comp_status_table
+
+    define("TICK", "../assets/icons/done-black-18dp.svg");
+    define("CROSS", "../assets/icons/close-black-18dp.svg");
+
+    //még nincsenek
+    
+    $query = "SELECT * FROM competitions WHERE comp_id = '$comp_id'";
+    $check_comp_query = mysqli_query($connection, $query);
+        
+     if($row = mysqli_fetch_assoc($check_comp_query)){
+        
+        $comp_host = $row["comp_host"];
+        $comp_info = $row["comp_info"];
+        $comp_wc_info = $row["comp_wc_info"];
+    }
+    $assoc_comp_table_elements = [
+        "g_invitations" => "",
+        "g_basic_info" => "",
+        "g_info_for_fencers" => "",
+        "g_timetable" => "",
+        "general" => "",
+        "t_referees" => "",
+        "t_technicians" => "",
+        "technical" => "",
+        "ranking" => "",
+        "pre_entries" => "",
+    ];
+    
+    $assoc_comp_table_elements['g_invitations'] = CROSS;
+    $assoc_comp_table_elements['pre_entries'] = CROSS;
+
+    //testing basic info
+    if ($comp_host != "") {
+        $assoc_comp_table_elements['g_basic_info'] = TICK;
+    } else {
+        $assoc_comp_table_elements['g_basic_info'] = CROSS;
+    }
+
+    //testing info for fencers
+    if ($comp_info != "") {
+        $assoc_comp_table_elements['g_info_for_fencers'] = TICK;
+    } else {
+        $assoc_comp_table_elements['g_info_for_fencers'] = CROSS;
+    }
+
+    //testing timetable
+    if ($comp_wc_info != "") {
+        $assoc_comp_table_elements['g_timetable'] = TICK;
+    } else {
+        $assoc_comp_table_elements['g_timetable'] = CROSS;
+    }
+    
+    //TESTING GENERAL___________________________________________________________________________________________
+
+    if ($assoc_comp_table_elements['g_basic_info'] == TICK && $assoc_comp_table_elements['g_basic_info'] == TICK && $assoc_comp_table_elements['g_timetable'] == TICK && $assoc_comp_table_elements['g_invitations'] == TICK) {
+        $assoc_comp_table_elements['general'] = TICK;
+    } else {
+        $assoc_comp_table_elements['general'] = CROSS;
+    }
+
+
+
+    //tesing refrees
+    $query_ref = "SELECT * FROM referees WHERE ass_comp_id regexp '(^|[[:space:]])$comp_id([[:space:]]|$)'";
+    $refnum = mysqli_query($connection, $query_ref);
+
+    if (mysqli_num_rows($refnum) != 0){
+        $assoc_comp_table_elements['t_referees'] = TICK;
+    } else {
+        $assoc_comp_table_elements['t_referees'] = CROSS;
+    }
+
+
+
+    //tesing technicians
+    $query_techn = "SELECT * FROM technicians WHERE ass_comp_id regexp '(^|[[:space:]])$comp_id([[:space:]]|$)'";
+    $technnum = mysqli_query($connection, $query_techn);
+
+    if (mysqli_num_rows($technnum) != 0){
+        $assoc_comp_table_elements['t_technicians'] = TICK;
+    } else {
+        $assoc_comp_table_elements['t_technicians'] = CROSS;
+    }
+
+    //TESTING TECHICAL__________________________________________________________________________________________
+
+    if ($assoc_comp_table_elements['t_referees'] == TICK && $assoc_comp_table_elements['t_technicians'] == TICK) {
+        $assoc_comp_table_elements['technical'] = TICK;
+    } else {
+        $assoc_comp_table_elements['technical'] = CROSS;
+    }
+
+    //tesing ranking
+    $query_techn = "SELECT * FROM ranking WHERE ass_comp_id regexp '(^|[[:space:]])$comp_id([[:space:]]|$)'";
+    $ranknum = mysqli_query($connection, $query_techn);
+
+    if (mysqli_num_rows($ranknum) != 0){
+        $assoc_comp_table_elements['ranking'] = TICK;
+    } else {
+        $assoc_comp_table_elements['ranking'] = CROSS;
+    }
+
+    //majd testing pre-entries ha kész lesz
+    
+    //if all of the above are TICK then the publish competition button will NOT be disabled
+    $publish_comp_disabled = "";
+    if (array_search(CROSS, $assoc_comp_table_elements, FALSE) !== FALSE) {
+        $publish_comp_disabled = " disabled";
+    }
 ?>
-<!-- ez miert nincs headerburgerben? -->
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -39,10 +151,13 @@ checkComp($connection);
                         <img src="../assets/icons/arrow_back_ios-black-18dp.svg"></img>
                     </button>
                     <p class="comp_title"><?php echo $comp_name; ?></p>
-                    <button class="stripe_button orange only">
+
+                    <form id="publishcomp" class="hidden" ></form>
+                    <button class="stripe_button orange only <?php echo $publish_comp_disabled ?>" id="publishcomp">
                         <p>Publish Competition</p>
                         <img src="../assets/icons/send-black-18dp.svg"></img>
                     </button>
+
                 </div>
 
                 <!-- dashboard body -->  
@@ -64,9 +179,54 @@ checkComp($connection);
                             <div class="db_panel">
                                 
                                 <!-- competition status panel header -->
-                                <?php include "../includes/comp_status.php"; ?>
+
+                                <?php //egyan az mint index.php elejen a comp_name lekérés
+                                    $query = "SELECT * FROM competitions WHERE comp_id = '$comp_id'";
+                                    $check_comp_query = mysqli_query($connection, $query);
+
+                                    if($row = mysqli_fetch_assoc($check_comp_query)){
+
+                                        $comp_status = $row["comp_status"];
+
+                                    }
+                                ?>  
+
+                                <div class="db_panel_title_stripe">
+                                    <img src="../assets/icons/beenhere-black-18dp.svg" alt="" class="db_panel_stripe_icon">
+                                    <p>Competition's status:</p><p class="dp_comp_status"><?php echo statusConverter($comp_status) ?></p>
+                                </div>
+
                                 <!-- competiton status table -->
-                                <?php include "../includes/comp_status_table.php" ?>
+                                <div class="db_panel_main">
+
+                                    <div id="sheduled_to_do_list">
+
+                                        <a class="sublist_title" onclick="toggle_general_to_do()">General<img src="<?php echo $assoc_comp_table_elements['general'] ?>" alt=""></a>
+                                            
+                                            <ul id="general_to_do" class="sheduled_to_do_sublist">
+
+                                                <li>Basic Information<img src="<?php echo $assoc_comp_table_elements['g_basic_info'] ?>" alt=""></li>
+                                                <li>Information for fencers<img src="<?php echo $assoc_comp_table_elements['g_info_for_fencers'] ?>" alt=""></li>
+                                                <li>Timetable<img src="<?php echo $assoc_comp_table_elements['g_timetable'] ?>" alt=""></li>
+                                                <li>Invitation<img src="<?php echo $assoc_comp_table_elements['g_invitations'] ?>" alt=""></li>
+
+                                            </ul>
+
+                                        <a class="sublist_title" onclick="toggle_technical_to_do()">Technical<img src="<?php echo $assoc_comp_table_elements['technical'] ?>" alt=""></a>
+                                        
+                                            <ul id="technical_to_do" class="sheduled_to_do_sublist">
+                                                
+                                                <li>Technicians<img src="<?php echo $assoc_comp_table_elements['t_technicians'] ?>" alt=""></li>
+                                                <li>Referees<img src="<?php echo $assoc_comp_table_elements['t_referees'] ?>" alt=""></li>
+                                            
+                                            </ul>
+
+                                        <a>Ranking<img src="<?php echo $assoc_comp_table_elements['ranking'] ?>" alt=""></a>
+
+                                        <a>Pre-entries<img src="<?php echo $assoc_comp_table_elements['pre_entries'] ?>" alt=""></a>
+
+                                    </div>
+                                </div>
                             </div>
                         </div>
 
