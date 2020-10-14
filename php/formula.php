c228740556d77db34f3fdbc31489fb1dd28239fa
WolframKristof <wolfram.kristof@gmail.com>
2020-10-14 20:04:10 +0200

    kis UX cuccok

--- php/formula.php
+++ php/formula.php
@@ -15,7 +15,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Template</title>
+    <title>Formula of {comp name}</title>
     <link rel="stylesheet" href="../css/mainstyle.css">
     <link rel="stylesheet" href="../css/basestyle.css">
 </head>
