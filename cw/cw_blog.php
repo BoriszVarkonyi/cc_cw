a993475677c785ca6985621afd968de87d89ed20
WolframKristof <wolfram.kristof@gmail.com>
2020-10-16 20:40:15 +0200

    pre_reg.php

---
+++ cw/cw_blog.php
@@ -0,0 +1,68 @@
+<?php include "cw_comp_getdata.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <link rel="stylesheet" href="../css/cw_mainstyle.css">
+    <link rel="stylesheet" href="../css/basestyle.css">
+</head>
+<body>
+    <div id="cw_main_full">
+        <div class="cw_panel_title_wrapper">
+            <p>BLOG</p>
+        </div>
+
+
+        <form id="browsing_bar">
+            <input type="text" class="hidden"> <!-- IF storing the search is nedded in text form-->
+            <input type="search" name="" id="" placeholder="Search by Title">
+            <input type="submit" value="Search">
+        </form>
+
+        <div class="cw_table_wrapper">
+
+            <div class="blog_article">
+                <p class="article_title">BLOG ARTICLE TITLE EPIC TITLE BLOG TITLE FOR CW 2020 READY COMPETITION ONLY &Ł$kXvvSEXUALS CAN TAKE PART</p>
+                <img src="../assets/img/fencers_bg.svg" alt="" class="article_image">
+                <div>
+                    <p class="article_brief">Wow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZ</p>
+                    <p class="article_post_date">POSTED: 12 / 20 / 2020</p>
+                </div>
+            </div>
+
+            <div class="blog_article">
+                <p class="article_title">BLOG ARTICLE TITLE EPIC TITLE BLOG TITLE FOR CW 2020 READY COMPETITION ONLY &Ł$kXvvSEXUALS CAN TAKE PART</p>
+                <img src="../assets/img/fencers_bg.svg" alt="" class="article_image">
+                <div>
+                    <p class="article_brief">Wow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZ</p>
+                    <p class="article_post_date">POSTED: 12 / 20 / 2020</p>
+                </div>
+            </div>
+
+            <div class="blog_article">
+                <p class="article_title">BLOG ARTICLE TITLE EPIC TITLE BLOG TITLE FOR CW 2020 READY COMPETITION ONLY &Ł$kXvvSEXUALS CAN TAKE PART</p>
+                <img src="../assets/img/fencers_bg.svg" alt="" class="article_image">
+                <div>
+                    <p class="article_brief">Wow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZ</p>
+                    <p class="article_post_date">POSTED: 12 / 20 / 2020</p>
+                </div>
+            </div>
+
+            <div class="blog_article">
+                <p class="article_title">BLOG ARTICLE TITLE EPIC TITLE BLOG TITLE FOR CW 2020 READY COMPETITION ONLY &Ł$kXvvSEXUALS CAN TAKE PART</p>
+                <img src="../assets/img/fencers_bg.svg" alt="" class="article_image">
+                <div>
+                    <p class="article_brief">Wow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZWow cringe reported to r/incels by libtards die i hell. eddit Will eat ypu alive loser. incel life please follow me i need SEXSZ</p>
+                    <p class="article_post_date">POSTED: 12 / 20 / 2020</p>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+<?php include "cw_footer.php"; ?>
+</body>
+<script src="../js/cw_main.js"></script>
+</html>
