1d14bf932569a2df9c91e093f0faddbacf4ba12f
WolframKristof <wolfram.kristof@gmail.com>
2020-10-23 16:19:12 +0200

    enyhe javítások

--- php/invitation.php
+++ php/invitation.php
@@ -107,17 +107,6 @@ $getdata_do = mysqli_query($connection, $qry_getdata);
                                         </div>
                                     </div>
                                 </form>
-
-                                <div class="entry table_row_wrapper" id="">
-                                    <div class="table_row" onclick="toggleEntry(this)">
-                                        <div class="table_item invitation">Hungarian Fencing Federation</div>
-                                    </div>
-                                    <div class="entry_panel collapsed">
-                                        <textarea name="" id=""></textarea>
-                                        <input type="text" class="hidden">
-                                        <button class="panel_submit">Save</button>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                     </div>
