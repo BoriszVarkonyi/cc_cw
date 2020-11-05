e4a66da72c2a303da5820b1da95fb5fb00ab26bb
WolframKristof <wolfram.kristof@gmail.com>
2020-11-05 23:14:34 +0100

    új oldalak

---
+++ cw/cw_confirm_pre_reg.php
@@ -0,0 +1,20 @@
+<?php include "../includes/db.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Confirm Pre-Registration</title>
+    <link rel="stylesheet" href="../css/cw_mainstyle.css">
+    <link rel="stylesheet" href="../css/basestyle.css">
+</head>
+<body id="basic_body">
+    <div class="basic_panel">
+        <p>Email has been sent to <b>hunfencing@hunfencing.hu</b> for verification!</p>
+        <p class="bold">Please check your emails and confirm your Pre-Registartion.</p>
+        <a class="go_back" href="cw_competition.php">Go back</a>
+    </div>
+</body>
+</html>
---
+++ cw/cw_confirmed_pre_reg.php
@@ -0,0 +1,19 @@
+<?php include "../includes/db.php"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Confirm Pre-Registration</title>
+    <link rel="stylesheet" href="../css/cw_mainstyle.css">
+    <link rel="stylesheet" href="../css/basestyle.css">
+</head>
+<body id="basic_body">
+    <div class="basic_panel">
+        <p>Your Pre-Registration for <b>Competitions name</b> has been <b class="red">disapproved.</b><b class="green">approved.</b></p>
+        <p class="bold">If you think the decisison is incorrect contact the organiser of the competition!</p>
+    </div>
+</body>
+</html>
