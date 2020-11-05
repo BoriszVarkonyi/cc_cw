bb889bdfd11aa0fac52fd5aa474dea86981f5ccf
MEEP0321 <polyak.attila56@gmail.com>
2020-11-05 20:14:58 +0100

    Meh

--- php/timetable.php
+++ php/timetable.php
@@ -234,7 +234,7 @@ if($query_do){
                 </div>
                 <div id="page_content_panel_main" class="no_scroll">
 
-                    <div id="set_wc_panel" class="overlay_panel_példa hidden">
+                    <div id="set_wc_panel" class="overlay_panel_timetable hidden">
                             <button class="panel_button"onclick="closeWcPanel()">
                                 <img src="../assets/icons/close-black-18dp.svg" alt="">
                             </button>
