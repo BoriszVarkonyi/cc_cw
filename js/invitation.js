19823c42fd795bb79454555a240c60d254379940
WolframKristof <wolfram.kristof@gmail.com>
2020-11-07 23:14:21 +0100

    invitational attilának

--- js/invitation.js
+++ js/invitation.js
@@ -1,16 +1,16 @@
 function toggleEntry(x) {
 
-    var tableRow = x;
-    var entry = tableRow.parentNode;
-    var entryPanel = tableRow.nextElementSibling;
+  var tableRow = x;
+  var entry = tableRow.parentNode;
+  var entryPanel = tableRow.nextElementSibling;
 
-    entry.classList.toggle("opened");
-    entryPanel.classList.toggle("collapsed");
+  entry.classList.toggle("opened");
+  entryPanel.classList.toggle("collapsed");
 
 }
 
 function printPage() {
-  window.print();
+window.print();
 }
 
 //Add entry button.
@@ -19,8 +19,8 @@ var addingEntryPanel = document.getElementById("adding_entry");
 
 //Toggles the classes.
 function hideNshow () {
-    addEntryPanel.classList.toggle("hidden");
-    addingEntryPanel.classList.toggle("hidden");
+  addEntryPanel.classList.toggle("hidden");
+  addingEntryPanel.classList.toggle("hidden");
 }
 //END
 
@@ -29,72 +29,72 @@ var uploadButton = document.querySelector(".panel_submit");
 document.getElementById("fileText").textContent = " ";
 
 input.addEventListener("input", function() {
-  if(input.value == "") {
-    uploadButton.disabled = true;
+if(input.value == "") {
+  uploadButton.disabled = true;
 
-  }
-  else {
-    uploadButton.disabled = false;
-    document.getElementById("fileText").textContent = input.value.replace(input.value.substring(0, input.value.lastIndexOf("\\")) + "\\", "");
-  }
+}
+else {
+  uploadButton.disabled = false;
+  document.getElementById("fileText").textContent = input.value.replace(input.value.substring(0, input.value.lastIndexOf("\\")) + "\\", "");
+}
 })
 
 /*
 Array.prototype.forEach.call( inputs, function( input ) {
-  var label = input.nextElementSibling,
-              labelVal = label.innerHTML;
+var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
 
-  input.addEventListener( 'change', function( e ) {
-    var fileName = '';
+input.addEventListener( 'change', function( e ) {
+  var fileName = '';
 
-    if ( this.files && this.files.length > 1 ) {
-      fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-    } else {
-      fileName = e.target.value.split( '\\' ).pop();
-    }
+  if ( this.files && this.files.length > 1 ) {
+    fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+  } else {
+    fileName = e.target.value.split( '\\' ).pop();
+  }
 
-    if ( fileName ) {
-      label.querySelector( 'span' ).innerHTML = fileName;
-    } else {
-      label.innerHTML = labelVal;
-    }
-  });
+  if ( fileName ) {
+    label.querySelector( 'span' ).innerHTML = fileName;
+  } else {
+    label.innerHTML = labelVal;
+  }
+});
 });
 */
 //END
 
 var oldentry;
 function toggleEntry(x) {
-    var tableRow = x;
-    var entry = tableRow.parentNode;
-    var entryPanel = tableRow.nextElementSibling;
-    var entrys = document.querySelectorAll(".entry");
-    
-    //Making every entry collapsed.
-    for (i = 0; i < entrys.length; i++) {
-        entrys[i].lastElementChild.classList.add("collapsed")
-        entrys[i].classList.remove("opened")
+  var tableRow = x;
+  var entry = tableRow.parentNode;
+  var entryPanel = tableRow.nextElementSibling;
+  var entrys = document.querySelectorAll(".entry");
+  
+  //Making every entry collapsed.
+  for (i = 0; i < entrys.length; i++) {
+      entrys[i].lastElementChild.classList.add("collapsed")
+      entrys[i].classList.remove("opened")
 
-    } 
-     //Checking if the oldentry var. equals the entry.
-    if(entry == oldentry){
-        //If yes then it adds opened, and remove collapsed.
-        entryPanel.classList.remove("collapsed");
-        entry.classList.add("opened");
+  } 
+   //Checking if the oldentry var. equals the entry.
+  if(entry == oldentry){
+      //If yes then it adds opened, and remove collapsed.
+      entryPanel.classList.remove("collapsed");
+      entry.classList.add("opened");
 
-    }
+  }
 
-    entry.classList.toggle("opened");
-    entryPanel.classList.toggle("collapsed");
-    //Checking if we clicked the same entry.
-    if(entry.classList.contains("opened")){
-        //If yes it saves the entry.
-        oldentry = entry;
-    }
-    else {
-        //If no it sets the oldentry var. undifened
-        oldentry = undefined
-    }
+  entry.classList.toggle("opened");
+  entryPanel.classList.toggle("collapsed");
+  //Checking if we clicked the same entry.
+  if(entry.classList.contains("opened")){
+      //If yes it saves the entry.
+      oldentry = entry;
+  }
+  else {
+      //If no it sets the oldentry var. undifened
+      oldentry = undefined
+  }
 }
 
-  
+
