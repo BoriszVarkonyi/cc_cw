4a85a54df375065cceadf7f1b862ec55c747fc76
kmarcell0 <kmarcell.kovi@gmail.com>
2020-11-26 18:17:32 +0100

    ref & tech no password

--- php/referees.php
+++ php/referees.php
@@ -58,15 +58,13 @@
         //get data from form
         $ref_name = $_POST['username'];
         $ref_full_name = $_POST['full_name'];
-        if (!$ref_pass = password_hash($_POST['password'], PASSWORD_DEFAULT)) {
-            echo "szar a password hashes cucc";
-        }
 
-        $_POST['password'] = "";
+
+        
 
         $ref_nat = $_POST['f_nat'];
 
-        if ($ref_name != "" && $ref_full_name != "" && $ref_nat != "" && $ref_pass != "") {
+        if ($ref_name != "" && $ref_full_name != "" && $ref_nat != "") {
             
             //test for existing row
             $qry_row_test = "SELECT * FROM $table_name WHERE name = '$ref_name'";
@@ -76,7 +74,7 @@
             $feedback['rtest'] = "ok!";
 
             if ($row_num == FALSE) {
-                $qry_insert = "INSERT INTO $table_name (name, full_name, pass, nat) VALUES ('$ref_name', '$ref_full_name', '$ref_pass', '$ref_nat')";
+                $qry_insert = "INSERT INTO $table_name (name, full_name, pass, nat) VALUES ('$ref_name', '$ref_full_name', '', '$ref_nat')";
                 if (mysqli_query($connection, $qry_insert)) {
                     $feedback['insert'] = "ok!";
                 } else {
@@ -156,11 +154,7 @@
                             <form class="overlay_panel_form" action="referees.php?comp_id=<?php echo $comp_id; ?>" method="POST" id="new_technician">
                                 <label for="username" >NAME</label>
                                 <input type="text" placeholder="Type the referees's name" class="username_input" name="username">
-                                <label for="password">PASSWORD</label>
-                                <div>
-                                <input type="password" placeholder="Type the referees's password" id="password_input" class="password_input" name="password">
-                                <button type="button" id="random_password_button" onclick="randomPassword()" ><img src="../assets/icons/shuffle-black-18dp.svg"></button>
-                                </div>
+                                
                                 <label for="full_name" >FULL NAME</label>
                                 <input type="text" placeholder="Type the referees's full name" id="full_name_input" class="full_name_input" name="full_name">
                                 <label>NATION / CLUB</label>
