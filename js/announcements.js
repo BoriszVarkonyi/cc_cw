771b87cfb67ddcb8a9bd84e13758ff263a23cc28
MEEP0321 <polyak.attila56@gmail.com>
2020-12-19 21:07:19 +0100

    Annoucments nyilas kezelés

--- js/announcements.js
+++ js/announcements.js
@@ -32,8 +32,60 @@ document.onkeyup=function(e){
         newAnn.click()
         titleBar.focus()
     }
-}     
+}
 
+var selectedElementIndexAnn = 0;
+document.onkeydown = (keyDownEvent) => {
+    //Arrow system, works only if search bar closed
+    if(keyDownEvent.key == "ArrowUp"){
+        var tableRows = document.querySelectorAll(".db_panel_main  .entry")
+        var forms =  document.querySelectorAll(".db_panel_main form")
+        if(selectedElementIndexAnn == 0) {
+            selectedElementIndexAnn++
+        }
+        selectedElementIndexAnn--
+        forms[selectedElementIndexAnn + 1].classList.add("collapsed")
+        tableRows[selectedElementIndexAnn + 1].classList.remove("opened")
+        tableRows[selectedElementIndexAnn + 1].blur()
+        forms[selectedElementIndexAnn].classList.remove("collapsed")
+        tableRows[selectedElementIndexAnn].classList.add("opened")
+        tableRows[selectedElementIndexAnn].focus()
+        keyDownEvent.preventDefault();
+    }
+
+
+    if(keyDownEvent.key == "ArrowDown"){
+        //Cheks if theres a selected row.
+        var hasSelected = false;
+        var tableRows = document.querySelectorAll(".db_panel_main  .entry")
+        var forms =  document.querySelectorAll(".db_panel_main form")
+        for(i=0; i<tableRows.length; i++) {
+            if(tableRows[i].classList.contains("opened")){
+                hasSelected = true
+                break;
+            }
+        }
+        if(hasSelected){
+            if(selectedElementIndexAnn == tableRows.length -1) {
+                selectedElementIndexAnn--
+            }
+            selectedElementIndexAnn++
+            forms[selectedElementIndexAnn - 1].classList.add("collapsed")
+            tableRows[selectedElementIndexAnn - 1].classList.remove("opened")
+            tableRows[selectedElementIndexAnn - 1].blur()
+            forms[selectedElementIndexAnn].classList.remove("collapsed")
+            tableRows[selectedElementIndexAnn].classList.add("opened")
+            tableRows[selectedElementIndexAnn].focus()
+            keyDownEvent.preventDefault();
+        }
+        //If there is not a selected row than it selects the first row.
+        else{
+            forms[selectedElementIndexAnn].classList.remove("collapsed")
+            tableRows[selectedElementIndexAnn].classList.add("opened")
+            tableRows[selectedElementIndexAnn].focus()
+        }
+    }
+}     
 
 
 
