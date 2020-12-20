9555c0e3c1e11cd8f873dad542f4de7c13c4f14b
MEEP0321 <polyak.attila56@gmail.com>
2020-12-20 21:59:28 +0100

    table resizers

--- js/weapon_control.js
+++ js/weapon_control.js
@@ -1,16 +1,42 @@
 var wctable = document.querySelector(".table_row_wrapper")
 var addWeaponControlButton = document.getElementById("wcButton")
-wctable.addEventListener("click", function(){
+var sendMessageButton = document.getElementById("sendMessageButton")
+addWeaponControlButton.disabled = true;
+sendMessageButton.disabled = true;
+addWeaponControlButton.classList.add("disabled")
+sendMessageButton.classList.add("disabled")
+function buttonDisabler(){
     var selectedItem = document.querySelector(".table_row_wrapper .selected")
     if(selectedItem !== null){
-        addWeaponControlButton.disabled = true;
-        addWeaponControlButton.classList.add("disabled")
+        addWeaponControlButton.disabled = false;
+        sendMessageButton.disabled = false;
+        addWeaponControlButton.classList.remove("disabled")
+        sendMessageButton.classList.remove("disabled")
     }
     else{
-        addWeaponControlButton.disabled = false;
-        addWeaponControlButton.classList.remove("disabled")
+        addWeaponControlButton.disabled = true;
+        sendMessageButton.disabled = true;
+        addWeaponControlButton.classList.add("disabled")
+        sendMessageButton.classList.add("disabled")
     }
-})
+}
+
+var wcRows = document.querySelectorAll(".table_row_wrapper .table_row")
+function callback(mutationsList, observer) {
+    mutationsList.forEach(mutation => {
+        if (mutation.attributeName === 'class') {
+            buttonDisabler()
+        }
+    })
+}
+    
+const mutationObserver2 = new MutationObserver(callback)
+for(i=0; i<wcRows.length; i++){
+mutationObserver2.observe(wcRows[i], { attributes: true })
+}
+
+wctable.addEventListener("click", buttonDisabler)    
+
 document.addEventListener("keyup", function(e){
     if(e.shiftKey && e.which == 78) {
         addWeaponControlButton.click()
