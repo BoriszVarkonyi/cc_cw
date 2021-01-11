758eb9a9a8a6545ee39a3988f0db7d2052f7b032
MEEP0321 <polyak.attila56@gmail.com>
2021-01-11 22:59:05 +0100

    short cut shift key

--- js/invitation.js
+++ js/invitation.js
@@ -75,10 +75,12 @@ function toggleEntry(x) {
   }
 }
 document.addEventListener("keyup", function(e){
+  //Saves to Shift+S
   if(e.shiftKey && e.which == 83) {
     var orangeSaveButton = document.querySelector(".stripe_button.orange")
     orangeSaveButton.click()
   }
+  //Prints to Shift+P
   if(e.shiftKey && e.which == 80) {
   var stripeButton = document.querySelector(".stripe_button")
   stripeButton.click()
