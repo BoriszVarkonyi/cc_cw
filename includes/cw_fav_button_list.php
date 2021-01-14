2380072a992fed7f02cf8b5d8bc735ae01b998ae
kmarcell0 <kmarcell.kovi@gmail.com>
2021-01-14 10:49:29 +0100

    c_fav_button_list hot fix

--- includes/cw_fav_button_list.php
+++ includes/cw_fav_button_list.php
@@ -3,29 +3,6 @@
     $cookie_expires = time() + 31556926;
     $cookie_name = "fav_comp";
 
-    //get comp_id
-    if (isset($_GET['comp_id'])) {
-        $comp_id = $_GET['comp_id'];
-
-        if (isset($_COOKIE[$cookie_name])) {
-            $cookie_value = $_COOKIE[$cookie_name];
-        } else {
-            $cookie_value = "%";
-        }
-    
-        $contains = strrpos($cookie_value, $comp_id . "%");
-    
-        if ($contains != FALSE) {
-            $contains = TRUE;
-        }
-    
-        if ($contains) {
-            $star = "../assets/icons/star-black-18dp.svg";
-        } else {
-            $star = "../assets/icons/star_border-black-18dp.svg";
-        }
-    }
-
     if (isset($_POST['submit_button'])) {
 
         
