6648eb77f5d0579418695ddf9281dc2be811b429
kmarcell0 <kmarcell.kovi@gmail.com>
2021-01-16 22:31:52 +0100

    Urunk szuletesehozzá lett adva a competitors.php hoz

--- cw/competitors.php
+++ cw/competitors.php
@@ -58,32 +58,19 @@
                     </div>
 
                     <div class="select_input dense">
-                        <input type="number" name="year" placeholder="-Year-" onclick="toggleDropdown(this)">
+                        <input type="number" name="year" placeholder="-Year-Of-Birth-" onclick="toggleDropdown(this)">
                         <div id="year_select_dropdown">
-                            <button type="button" onclick="selectSystem(this)">2020</button>
-                            <button type="button" onclick="selectSystem(this)">2019</button>
-                            <button type="button" onclick="selectSystem(this)">2018</button>
-                            <button type="button" onclick="selectSystem(this)">2017</button>
-                            <button type="button" onclick="selectSystem(this)">2016</button>
-                            <button type="button" onclick="selectSystem(this)">2015</button>
-                            <button type="button" onclick="selectSystem(this)">2014</button>
-                            <button type="button" onclick="selectSystem(this)">2013</button>
-                            <button type="button" onclick="selectSystem(this)">2012</button>
-                            <button type="button" onclick="selectSystem(this)">2011</button>
-                            <button type="button" onclick="selectSystem(this)">2010</button>
-                            <button type="button" onclick="selectSystem(this)">2009</button>
-                            <button type="button" onclick="selectSystem(this)">2008</button>
-                            <button type="button" onclick="selectSystem(this)">2007</button>
-                            <button type="button" onclick="selectSystem(this)">2006</button>
-                            <button type="button" onclick="selectSystem(this)">2005</button>
-                            <button type="button" onclick="selectSystem(this)">2004</button>
-                            <button type="button" onclick="selectSystem(this)">2003</button>
-                            <button type="button" onclick="selectSystem(this)">2002</button>
-                            <button type="button" onclick="selectSystem(this)">2001</button>
-                            <button type="button" onclick="selectSystem(this)">2000</button>
-                            <button type="button" onclick="selectSystem(this)">1999</button>
-                            <button type="button" onclick="selectSystem(this)">1998</button>
-                            <button type="button" onclick="selectSystem(this)">1997</button>
+                            <?php
+                                for ($i = 0; $i <= date("Y"); $i++) {
+                                    if ($i == date("Y")) {
+                                        $year = "URUNK KIRÁLYUNK JÉZUS KRISZTUS SZÜLETÉSE";
+                                    } else {
+                                        $year = date("Y") - $i; 
+                                    }
+                                    ?><button type="button" onclick="selectSystem(this)"><?php echo $year ?></button><?php
+                                }
+
+                            ?>
                         </div>
                     </div>
                     <input name="submit_search" type="submit" value="Search">
