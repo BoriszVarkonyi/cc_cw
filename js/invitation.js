7e0378682c0caaf97d877a634c73f82f8c73b80f
MEEP0321 <polyak.attila56@gmail.com>
2021-01-19 22:26:00 +0100

    invitation arrow system

--- js/invitation.js
+++ js/invitation.js
@@ -48,30 +48,35 @@ function toggleEntry(x) {
   var tableRow = x;
   var entry = tableRow.parentNode;
   var entryPanel = tableRow.nextElementSibling;
-  var entrys = document.querySelectorAll(".entry");
-  
+  var entrys =  document.querySelectorAll(".entry");
+
   //Making every entry collapsed.
   for (i = 0; i < entrys.length; i++) {
-    entrys[i].lastElementChild.classList.add("collapsed")
-    entrys[i].classList.remove("opened")
+      entrys[i].lastElementChild.classList.add("collapsed")
+      entrys[i].classList.remove("opened")
+      if(entrys[i] == entry){
+          selectedElementIndexAnn = i;
+      }
+
   } 
-   //Checking if the oldentry var. equals the entry.
+  //Checking if the oldentry var. equals the entry.
   if(entry == oldentry){
-    //If yes then it adds opened, and remove collapsed.
-    entryPanel.classList.remove("collapsed");
-    entry.classList.add("opened");
+      //If yes then it adds opened, and remove collapsed.
+      entryPanel.classList.remove("collapsed");
+      entry.classList.add("opened");
+      selectedElementIndexAnn = 0;
   }
 
   entry.classList.toggle("opened");
   entryPanel.classList.toggle("collapsed");
   //Checking if we clicked the same entry.
   if(entry.classList.contains("opened")){
-    //If yes it saves the entry.
-    oldentry = entry;
+      //If yes it saves the entry.
+      oldentry = entry
   }
   else {
-    //If no it sets the oldentry var. undifened
-    oldentry = undefined;
+      //If no it sets the oldentry var. undifened
+      oldentry = undefined
   }
 }
 document.addEventListener("keyup", function(e){
@@ -86,3 +91,69 @@ document.addEventListener("keyup", function(e){
   stripeButton.click()
 }
 })
+
+var IsNotFocused = true;
+var openedForm =  document.querySelectorAll(".db_panel_main .entry_panel textarea")
+openedForm.forEach(item => { 
+    item.addEventListener("focus", function(){
+        IsNotFocused = false;
+    })
+    item.addEventListener("blur", function(){
+        IsNotFocused = true;
+    })
+})
+
+var selectedElementIndexAnn = 0;
+document.onkeydown = (keyDownEvent) => {
+    //Arrow system
+    if(IsNotFocused){
+        if(keyDownEvent.key == "ArrowUp"){
+            var tableRows = document.querySelectorAll(".db_panel_main  .entry")
+            var forms =  document.querySelectorAll(".db_panel_main form")
+            if(selectedElementIndexAnn == 0) {
+                selectedElementIndexAnn++
+            }
+            selectedElementIndexAnn--
+            forms[selectedElementIndexAnn + 1].classList.add("collapsed")
+            tableRows[selectedElementIndexAnn + 1].classList.remove("opened")
+            tableRows[selectedElementIndexAnn + 1].blur()
+            forms[selectedElementIndexAnn].classList.remove("collapsed")
+            tableRows[selectedElementIndexAnn].classList.add("opened")
+            tableRows[selectedElementIndexAnn].focus()
+            keyDownEvent.preventDefault();
+        }
+
+
+        if(keyDownEvent.key == "ArrowDown"){
+            //Cheks if theres a selected row.
+            var hasSelected = false;
+            var tableRows = document.querySelectorAll(".db_panel_main  .entry")
+            var forms =  document.querySelectorAll(".db_panel_main form")
+            for(i=0; i<tableRows.length; i++) {
+                if(tableRows[i].classList.contains("opened")){
+                    hasSelected = true
+                    break;
+                }
+            }
+            if(hasSelected){
+                if(selectedElementIndexAnn == tableRows.length -1) {
+                    selectedElementIndexAnn--
+                }
+                selectedElementIndexAnn++
+                forms[selectedElementIndexAnn - 1].classList.add("collapsed")
+                tableRows[selectedElementIndexAnn - 1].classList.remove("opened")
+                tableRows[selectedElementIndexAnn - 1].blur()
+                forms[selectedElementIndexAnn].classList.remove("collapsed")
+                tableRows[selectedElementIndexAnn].classList.add("opened")
+                tableRows[selectedElementIndexAnn].focus()
+                keyDownEvent.preventDefault();
+            }
+            //If there is not a selected row than it selects the first row.
+            else{
+                forms[selectedElementIndexAnn].classList.remove("collapsed")
+                tableRows[selectedElementIndexAnn].classList.add("opened")
+                tableRows[selectedElementIndexAnn].focus()
+            }
+        }
+    }    
+} 
