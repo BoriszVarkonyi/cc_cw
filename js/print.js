a73b41ed3017eb732ee56863cad7f79688f568f9
MEEP0321 <polyak.attila56@gmail.com>
2021-01-29 23:16:26 +0100

    check in fencer shortcuts

---
+++ js/print.js
@@ -0,0 +1,31 @@
+var paperWrapper = document.querySelector(".paper_wrapper");
+var zoomNumber = 0.5;
+
+function zoomOut(){
+    zoomNumber = zoomNumber - 0.1
+    papertWrapper.style.transform = "scale(" + zoomNumber + ")";
+    zoomButtonDisabler();
+}
+function zoomIn(){
+    zoomNumber = zoomNumber + 0.1
+    paperWrapper.style.transform = "scale(" + zoomNumber + ")";
+    zoomButtonDisabler();
+}
+
+var zoomOutButton = document.getElementById("zoomOutButton")
+var zoomInButton = document.getElementById("zoomInButton")
+
+function zoomButtonDisabler(){
+    if(paperWrapper.style.transform == "scale(0.1)"){
+        zoomOutButton.disabled = true;
+    }
+    else{
+        zoomOutButton.disabled = false;
+    }
+    if(paperWrapper.style.transform == "scale(2)"){
+        zoomInButton.disabled = true;
+    }
+    else{
+        zoomInButton.disabled = false;
+    }
+}
---
+++ js/weapon_control_administrated.js
@@ -0,0 +1,37 @@
+//var wcStatistics = document.querySelector(".stripe_button.blue");
+//var sendMessangeButton = document.getElementById("sendMessageButton");
+var checkInButton = document.getElementById("checkInButton");
+var checkOutButton = document.getElementById("checkOutButton");
+var addwcButton = document.getElementById("addWcButton");
+var editWcButton = document.getElementById("editWcButton");
+//Hides all the button
+function hideAllButton(){
+    checkInButton.style.display = "none";
+    checkOutButton.style.display = "none";
+    addwcButton.style.display = "none";
+    editWcButton.style.display = "none";  
+}
+hideAllButton();
+//Shows the correct button
+function buttonShower(x){
+    var selectedItem = x
+    var className = selectedItem.classList[1]
+    switch(className){
+        case "cheked_out" :
+            hideAllButton();
+        break;
+        case "not_cheked_out" :
+            hideAllButton();
+            editWcButton.style.display = "";
+            checkOutButton.style.display = ""; 
+        break;
+        case "not_ready" :
+            hideAllButton();
+            addwcButton.style.display = "";
+        break;
+        case "red" :
+            hideAllButton();
+            checkInButton.style.display = "";
+        break;
+    }
+}
