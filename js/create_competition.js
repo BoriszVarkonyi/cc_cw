7506aec92249082529107564083b2bb1f8edba5b
MEEP0321 <polyak.attila56@gmail.com>
2021-02-02 22:28:27 +0100

    create comp form validation

--- js/create_competition.js
+++ js/create_competition.js
@@ -7,17 +7,36 @@ var valid1 = false, valid2 =false;
 createButton.classList.add("disabled")
 function crtTformValidation(){
     if(titleInput.value == ""){
-        valid2 = false;
+        valid1 = false;
     }
     else{
         valid1 = true;
     }
+    valid2 = true;
     for(i=0; i<optionContainers.length; i++){
         var options = optionContainers[i].querySelectorAll("input")
-        
+        if(valid2){
+            for(k=0; k<options.length; k++){
+                if(options[k].checked){
+                    valid2 = true;
+                    break;
+                }
+                else{
+                    valid2 = false;
+                }
+            }
+        }
+    }
+    if(valid1 && valid2){
+        createButton.classList.remove("disabled")
+    }
+    else{
+        createButton.classList.add("disabled")
     }
 }
+
 form.addEventListener("input", crtTformValidation)
+
 function errorChecker(x){
     if(x.value == ""){
         x.previousElementSibling.classList.add("error")
