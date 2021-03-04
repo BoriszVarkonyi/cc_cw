553b122deb1ec345e5f465933518689969e35005
WolframKristof <wolfram.kristof@gmail.com>
2021-03-04 17:58:29 +0100

    some  CL and EI fixed 2

--- includes/headerburger.php
+++ includes/headerburger.php
@@ -200,12 +200,12 @@ if (isset($_POST["logout"])) {
 
     <!-- profile panel drop-down -->
     <div id="profile_panel" class="small overlay_panel hidden">
+        <a class="panel_button left" href="your_profile.php" target="_blank" aria-label="Check and edit your profile">
+            <img src="../assets/icons/edit-black-18dp.svg"/>
+        </a>
         <button class="panel_button" onclick="toggle_profile_panel()">
             <img src="../assets/icons/close-black-18dp.svg"/>
         </button>
-        <a class="panel_button left" href="your_profile.php" target="_blank">
-            <img src="../assets/icons/edit-black-18dp.svg"/>
-        </a>
         <img src="<?php echo $profile_pic ?>" class="profile_picture_big" height="55" width="55">
         <p class="username_big"><?php echo $username; ?></p>
         <p class="role_big"><?php echo $role; ?></p>
