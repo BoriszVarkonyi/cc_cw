1ca030870d0ae3dff2d35ba22e5de76141d5c790
WolframKristof <wolfram.kristof@gmail.com>
2021-03-07 20:31:05 +0100

    Login chat done beta ugly

---
+++ js/login_chat.js
@@ -0,0 +1,18 @@
+var dropdownButton = document.getElementById("dropdown_button");
+var dropdownInput = document.getElementById("competition");
+var dropdownInputId = document.getElementById("competition_id");
+var dropdownMenu = document.getElementById("dropdown_menu")
+
+function dropDown() {
+    dropdownButton.classList.toggle("focus");
+    dropdownMenu.classList.toggle("focus");
+}
+
+function selectSystem(x) {
+    var selectedItem = x;
+    dropdownInput.value = selectedItem.innerText;
+    dropdownInputId.value = selectedItem.id;
+
+    dropdownButton.classList.toggle("focus");
+    dropdownMenu.classList.toggle("focus");
+}
--- php/chat.php
+++ php/chat.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Chat</title>
-    <link rel="stylesheet" href="../css/basestyle.min.css">
     <link rel="stylesheet" href="../css/chat_style.min.css">
 </head>
 <body>
