976da61f0ea8ca8573b31418a0ee661695b9468c
kmarcell0 <kmarcell.kovi@gmail.com>
2021-03-07 20:30:02 +0100

    json_pools is now done

--- random_geci/pools_json.php
+++ random_geci/pools_json.php
@@ -8,37 +8,83 @@
 
     $number_of_pools = 41;
 
-    class pool{
-        public $nationalitys = [];
-    }
 
-    class fencer_to_add {
-        public $prenom_nom;
-        public $id;
-        public $c_pos;
-        public $r_pos;
-        public $nation;
-
-        function __construct(object $fencer_obj){
-            $this -> prenom_nom = $fencer_obj->prenom . " " . $fencer_obj->nom;
-            $this -> id = $fencer_obj->id;
-            $this -> c_pos = $fencer_obj->comp_rank;
-            $this -> r_pos = $fencer_obj->classement;
-            $this -> nation = $fencer_obj->nation;
-        }
-    }
-
-
-
+    //1. param: hány darab csoport van,
+    //2. param: hány személyesek a csoportok,
+    //3. param: a versenyzők tömbje (sortolva, versenyzők objectek egy tömbben)
     function sorolas(int $n_pools,int $fencers_in_pools,array $array_of_fencers) {
 
-        //function amivel hozzárakjuk a fencereket
-        function add_fencer(int $x,int $y,object $fencer_obj,array $array_of_pools) {
-            $object = new fencer_to_add($fencer_obj);
-            $array_of_pools[$x] -> $y = $object;
-            array_push($array_of_pools[$x] -> nationalitys,$fencer_obj -> nation);
+        //kicsit szukitett fencer csak a necessery info van benne
+        class fencer {
+            public $prenom_nom;
+            public $id;
+            public $c_pos;
+            public $r_pos;
+            public $nation;
 
-            return $array_of_pools;
+            function __construct(object $fencer_obj){
+                $this -> prenom_nom = $fencer_obj->prenom . " " . $fencer_obj->nom;
+                $this -> id = $fencer_obj->id;
+                $this -> c_pos = $fencer_obj->comp_rank;
+                $this -> r_pos = $fencer_obj->classement;
+                $this -> nation = $fencer_obj->nation;
+            }
+        }
+
+        //needs 'fencer' class before!
+        //class of pools (fencer inside this)
+        class pool{
+            public $nationalitys = [];
+
+            //function amivel hozzárakjuk a fencereket
+            function add_fencer(int $y,object $fencer_obj) {
+
+                $fencer_to_add = new fencer($fencer_obj);
+
+                $this -> {$y} = $fencer_to_add;
+                array_push($this -> nationalitys,$fencer_obj -> nation);
+            }
+        }
+
+        //forward object saves xy when going ahead or backwards
+        class forward {
+            public $x = NULL;
+            public $y = NULL;
+            public $n_pools;
+
+            //just so we know the number of pools
+            function __construct($n_pools) {
+                $this -> n_pools = $n_pools;
+            }
+
+            //saving position before going ahead
+            function savePos($x, $y) {
+                $n_pools = $this -> n_pools;
+                if ($y % 2 == 0) {
+                    //even
+                    $x++;
+                } else {
+                    //odd
+                    $x--;
+                }
+                $this -> x = $x;
+                $this -> y = $y;
+            }
+
+            //sets saved x y to NULL
+            function reset() {
+                $this -> x = NULL;
+                $this -> y = NULL;
+            }
+
+            //tests wether xy is NULL if so returns false
+            function isSaved() {
+                if ($this -> x === NULL && $this -> y === NULL) {
+                    return false;
+                } else {
+                    return true;
+                }
+            }
         }
 
         //poolok letrehozasa az arrayben
@@ -50,15 +96,18 @@
         }
 
         //counterek
-        $fw_counter = 0;
+        $forward = new forward($n_pools);
         $fencer = 0;
 
         //INITIATE KíGYÓZÁS
         for ($y = 1; $y <= $fencers_in_pools; $y++) {
             if ($y % 2 == 0) {
                 //EVEN 2, 4, 6
-                for ($x = $n_pools; $x >= 1; $x--) {
-
+                for ($x = $n_pools; $x >= 1 && $y % 2 == 0; $x--) {
+                    //letezik e a kovi fencer
+                    if (!isset($array_of_fencers[$fencer])) {
+                        break 2;
+                    }
                     //van e mar fencer beirva
                     if (!isset($array_of_pools[$x] -> {$y})) {
 
@@ -66,63 +115,32 @@
                         if (array_search($array_of_fencers[$fencer] -> nation,$array_of_pools[$x] -> nationalitys) === FALSE) {
 
                             //fencer beiras majd fencer +1
-                            $array_of_pools = add_fencer($x,$y,$array_of_fencers[$fencer], $array_of_pools);
+                            $array_of_pools[$x] -> add_fencer($y, $array_of_fencers[$fencer]);
                             $fencer++;
-                            //letezik e a kövi fencer
-                            if (!isset($array_of_fencers[$fencer])) {
-                                break 2;
-                            }
 
-                            //vissza kell e lepni
-                            if ($fw_counter != 0) {
-                                //visszaleptetes
-                                $quotient = $fw_counter / $n_pools;
-                                $y = $y - floor($quotient);
-                                $fw_counter = 0;
-                                if ($y % 2 == 0) {
-                                    $x = $n_pools + 1;
-                                } else {
-                                    $x = 1;
+                            //visszalepes
+                            if ($forward -> isSaved()) {
+                                $x = $forward -> x;
+                                $y = $forward -> y;
+                                $forward -> reset();
+                                if ($y % 2 != 0) { // ha y paratlan átlép a másik ágba
+                                    $y--;
+                                    break;
                                 }
                             }
-                        } else { //ha van mar ilyen nat a poolban
-                            if ($x == $n_pools  && $y == $fencers_in_pools) { //ha a vegere ertunk akkor pakoljuk be az eredeti pos ba
-                                $remainder = $fw_counter % $n_pools;
-                                $quotient = $fw_counter / $n_pools;
-
-                                $y = $fencers_in_pools - floor($quotient);
-                                if ($y % 2 == 0) {
-                                    for ($x = $n_pools; $x >= 1; $x--) {
-                                        if (!isset($array_of_pools[$x] -> {$y})) {
-                                            $array_of_pools = add_fencer($x,$y,$array_of_fencers[$fencer], $array_of_pools);
-                                            break;
-                                        }
-                                    }
-                                } else {
-                                    for ($x = 1; $x <= $n_pools; $x++) {
-                                        if (!isset($array_of_pools[$x] -> {$y})) {
-                                            $array_of_pools = add_fencer($x,$y,$array_of_fencers[$fencer], $array_of_pools);
-                                            break;
-                                        }
-                                    }
-                                }
-
-                                $fencer++;
-                                if (!isset($array_of_fencers[$fencer])) {
-                                    break 2;
-                                }
-                                $fw_counter = 0;
-
-                            } else { //elore leptetjuk a pointert meg az fw_countert
-                                $fw_counter++;
-                            }
+                        } else if (!$forward -> isSaved()) { //ha van mar ilyen nat a poolban
+                            $forward -> savePos($x, $y);
                         }
                     }
                 }
+
             } else {
                 //ODD 1, 3, 5, 7
-                for ($x = 1; $x <= $n_pools; $x++) {
-
+                for ($x = 1; $x <= $n_pools && $y % 2 != 0; $x++) {
+                    //letezik e a kovi fencer
+                    if (!isset($array_of_fencers[$fencer])) {
+                        break 2;
+                    }
                     //van e mar fencer beirva
                     if (!isset($array_of_pools[$x] -> {$y})) {
 
@@ -130,68 +148,59 @@
                         if (array_search($array_of_fencers[$fencer] -> nation,$array_of_pools[$x] -> nationalitys)  === FALSE) {
 
                             //fencer beiras majd fencer +1
-                            $array_of_pools = add_fencer($x,$y,$array_of_fencers[$fencer], $array_of_pools);
+                            $array_of_pools[$x] -> add_fencer($y, $array_of_fencers[$fencer]);
                             $fencer++;
+                            //visszalepes
+                            if ($forward -> isSaved()) {
+                                $x = $forward -> x;
+                                $y = $forward -> y;
 
-                            //letezik e a kovi fencer
-                            if (!isset($array_of_fencers[$fencer])) {
-                                break 2;
-                            }
-
-                            //kell e vissza lepni
-                            if ($fw_counter != 0) {
-                                //visszaleptetes
-                                $quotient = $fw_counter / $n_pools;
-                                $y = $y - floor($quotient);
-                                $remainder = $fw_counter % $n_pools;
-                                if ($y % 2 == 0) {
-                                    $x = $n_pools + 1;
-                                } else {
-                                    $x = 1;
+                                $forward -> reset();
+                                if ($y % 2 == 0) {// ha y paros átlép a másik ágba
+                                    $y--;
+                                    break;
                                 }
-                                $fw_counter = 0;
                             }
                         } else { //ha van mar ilyen nat a poolban
-
-                            if ($x >= $n_pools  && $y >= $fencers_in_pools) { //ha a vegere ertunk akkor pakoljuk be az eredeti pos ba
-                                $remainder = $fw_counter % $n_pools;
-                                $quotient = $fw_counter / $n_pools;
-
-                                $y = $fencers_in_pools - floor($quotient);
-                                if ($y % 2 == 0) {
-                                    for ($x = $n_pools; $x >= 1; $x--) {
-                                        if (!isset($array_of_pools[$x] -> {$y})) {
-                                            $array_of_pools = add_fencer($x,$y,$array_of_fencers[$fencer], $array_of_pools);
-                                            break;
-                                        }
+                            if ($x == $n_pools  && $y == $fencers_in_pools) { //ha a vegere ertunk akkor pakoljuk be az eredeti pos ba
+                                if ($forward -> isSaved()) {
+                                    $x = $forward -> x;
+                                    $y = $forward -> y;
+                                    if ($y % 2 == 0) {
+                                        //even
+                                        $x--;
+                                    } else {
+                                        //odd
+                                        $x++;
+                                    }
+                                    $array_of_pools[$x] -> add_fencer($y, $array_of_fencers[$fencer]);
+                                    $fencer++;
+                                    $forward -> reset();
+                                    if ($y % 2 == 0) {// ha y paros átlép a másik ágba
+                                        $y--;
+                                        break;
                                     }
                                 } else {
-                                    for ($x = 1; $x <= $n_pools; $x++) {
-                                        if (!isset($array_of_pools[$x] -> {$y})) {
-                                            $array_of_pools = add_fencer($x,$y,$array_of_fencers[$fencer], $array_of_pools);
-                                            break;
-                                        }
-                                    }
+                                    $array_of_pools[$n_pools] -> add_fencer($fencers_in_pools, $array_of_fencers[$fencer]);
+                                    $fencer++;
                                 }
-                                $fencer++;
-                                if (!isset($array_of_fencers[$fencer])) {
-                                    break 2;
-                                }
-                                $fw_counter = 0;
+                                //UTOLSO NEM CSAK PARATLAN LEHET LEHETNE PAROS IS
+
                             } else { //elore leptetjuk a pointert meg az fw_countert
-                                $fw_counter++;
+                                if (!$forward -> isSaved()) {
+                                    $forward -> savePos($x, $y);
+                                }
                             }
                         }
                     }
                 }
             }
         }
-
         return $array_of_pools;
     }
 
+    $array_of_pos = sorolas(8, 7, $array_of_fencers);
 
-    $array_of_pos = sorolas(41, 7, $array_of_fencers);
 ?>
 
 <!DOCTYPE html>
@@ -217,6 +226,7 @@
         echo "<br>";
     }
     echo "number of fencers: " .  $n_fencers;
+    print_r($array_of_pos);
 ?>
 </body>
 </html>
