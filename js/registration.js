90bc8e213278ea2c023f33c85de8ceff283eff4a
MEEP0321 <polyak.attila56@gmail.com>
2021-05-06 20:38:29 +0200

    registration

--- js/registration.js
+++ js/registration.js
@@ -78,3 +78,66 @@ for (i = 0; i < nat.length; i++) {
         nat[i].parentNode.parentNode.style.display = "none";
     }
 }
+
+//Add fencer overlay button and title system
+var leftDivisionButton = document.getElementById("overlayPanelButtonLeft");
+var rightDivisionButton = document.getElementById("overlayPanelButtonRight");
+var currentTitle = document.querySelector(".overlay_panel_controls p");
+var overlayPanelDivisions = document.querySelectorAll(".overlay_panel_division");
+var controlsCounter = document.querySelector(".overlay_panel_controls_counter");
+var maxNumber = overlayPanelDivisions.length;
+var currentDivision = 0;
+
+leftDivisionButton.disabled = true;
+currentTitle.innerHTML = overlayPanelDivisions[currentDivision].getAttribute("overlay_division_title")
+controlsCounter.innerHTML = maxNumber + " / " + (currentDivision + 1);
+
+function leftButton(x) {
+    currentDivision--
+    overlayPanelDivisions[currentDivision + 1].classList.remove("visible")
+    overlayPanelDivisions[currentDivision].classList.add("visible")
+    currentTitle.innerHTML = overlayPanelDivisions[currentDivision].getAttribute("overlay_division_title")
+    controlsCounter.innerHTML = maxNumber + " / " + (currentDivision + 1);
+    divisionButtonDisabler();
+}
+
+function rightButton(x) {
+    currentDivision++
+    overlayPanelDivisions[currentDivision - 1].classList.remove("visible");
+    overlayPanelDivisions[currentDivision].classList.add("visible");
+    currentTitle.innerHTML = overlayPanelDivisions[currentDivision].getAttribute("overlay_division_title");
+    controlsCounter.innerHTML = maxNumber + " / " + (currentDivision + 1);
+    divisionButtonDisabler();
+}
+
+function divisionButtonDisabler() {
+    if (currentDivision == 0) {
+        leftDivisionButton.disabled = true;
+    }
+    else {
+        leftDivisionButton.disabled = false;
+    }
+
+    if (currentDivision == maxNumber - 1) {
+        rightDivisionButton.disabled = true;
+    }
+    else {
+        rightDivisionButton.disabled = false;
+    }
+}
+
+//Club Nation deleter
+var clubAndNationInput = document.querySelectorAll("#set_nation_input")
+clubAndNationInput.forEach(item => {
+    item.addEventListener("keyup", function (e) {
+        var ul = item.nextElementSibling.nextElementSibling;
+        if (ul.classList.contains("empty")) {
+            item.value = "";
+            ul.classList.remove("empty")
+            ul.classList.add("error")
+        }
+        else {
+            ul.classList.remove("error")
+        }
+    })
+})
