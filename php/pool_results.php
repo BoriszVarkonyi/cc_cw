ce2ca2d27313244d8d178bf30cd63b2e29d22212
kmarcell0 <kmarcell.kovi@gmail.com>
2021-05-11 22:02:43 +0200

    disq attributes pool_results.php

--- php/pool_results.php
+++ php/pool_results.php
@@ -401,8 +401,10 @@
 
                                     //changable class name
                                     $class_cancel = "";
+                                    $disq_atr = "";
                                     if (isDisqualified($f1_score) || isDisqualified($f2_score)) {
                                         $class_cancel = " canceled"; //not her majesty's english |:(
+                                        $disq_atr = "readonly tabindex='-1'";
                                     }
                             ?>
                             <div class="match small_scroll <?php echo $szin = ($f1_score == 0 ? "red" : "green"); echo $class_cancel?>">
@@ -412,7 +414,7 @@
                                 <div>
                                     <p><?php echo $f1_name ?></p>
                                     <div>
-                                        <input type="text" form="savepool" placeholder="#" name="<?php echo $array_match_ids[0] . "-" . $array_match_ids[1] ?>" id="f1_sc" class="number_input" value="<?php echo $f1_score ?>">
+                                        <input <?php echo $disq_atr ?> type="text" form="savepool" placeholder="#" name="<?php echo $array_match_ids[0] . "-" . $array_match_ids[1] ?>" id="f1_sc" class="number_input" value="<?php echo $f1_score ?>">
                                         <input type="radio" name="<?php echo $match_number ?>" id="<?php echo "1," . $match_number ?>" value="id" disabled/>
                                         <label for="<?php echo "1," . $match_number ?>" class="collapsed">Winner</label>
                                     </div>
@@ -422,7 +424,7 @@
                                 </div>
                                 <div>
                                     <div>
-                                        <input type="text" form="savepool" placeholder="#" name="<?php echo $array_match_ids[1] . "-" . $array_match_ids[0] ?>" id="f2_sc" class="number_input" value="<?php echo $f2_score ?>">
+                                        <input <?php echo $disq_atr ?> type="text" form="savepool" placeholder="#" name="<?php echo $array_match_ids[1] . "-" . $array_match_ids[0] ?>" id="f2_sc" class="number_input" value="<?php echo $f2_score ?>">
                                         <input type="radio" name="<?php echo $match_number ?>" id="<?php echo "2," . $match_number ?>" value="enemy" disabled/>
                                         <label for="<?php echo "2," . $match_number ?>" class="collapsed">Winner</label>
                                     </div>
