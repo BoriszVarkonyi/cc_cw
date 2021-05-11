c53673a8576117e7c9a2f08fd4dfce5aa1aba1f5
MEEP0321 <polyak.attila56@gmail.com>
2021-05-11 23:11:52 +0200

    sort dydtem

--- js/list_search.js
+++ js/list_search.js
@@ -84,16 +84,54 @@ radioButtons.forEach(item => {
     });
 })
 
-function sortButton(x, mode) {
+//Sort system
+var defaultArray = [];
+var defaultNameSequence = document.querySelectorAll(".table_row .table_item:first-of-type p")
+for (i = 0; i < defaultNameSequence.length; i++) {
+    defaultArray.push(defaultNameSequence[i].innerHTML)
+}
+
+
+var allButtons = document.querySelectorAll(".table_header_text > button:first-of-type");
+var sortButtonCookie = cookieFinder("sortCookie", "")
+for(i= 0; i<sortButtonCookie[1]; i++){
+    sortButton(allButtons[sortButtonCookie[0]])
+}
+
+function sortButton(x) {
     //Gets the column index
-    var columnIndex
-    var allButtons = document.querySelectorAll(".table_header_text > button:first-of-type")
+    var columnIndex;
     for (i = 0; i < allButtons.length; i++) {
         if (allButtons[i] === x) {
             columnIndex = i
+
+        }
+        else {
+            allButtons[i].querySelector("img").src = "../assets/icons/switch_full_black.svg"
         }
     }
-    rowSort(columnIndex + 1);
+    //Handles the modes
+    var sortImg = x.querySelector("img")
+    var imgSoure = sortImg.src.replace(sortImg.src.substring(0, sortImg.src.lastIndexOf("/")) + "/", "");
+    switch (imgSoure) {
+        case "switch_down_black.svg":
+            // Current: A-Z Swtiches to: Z-A
+            sortImg.src = "../assets/icons/switch_up_black.svg"
+            rowSort(columnIndex + 1, "Z-A");
+            document.cookie = "sortCookie=" + columnIndex + "2"
+            break;
+        case "switch_up_black.svg":
+            // Current: Z-A Swtiches to: Default
+            sortImg.src = "../assets/icons/switch_full_black.svg"
+            rowSort(columnIndex + 1, "Default");
+            document.cookie = "sortCookie=";
+            break;
+        default:
+            // Current: Default Swtiches to: A-Z
+            sortImg.src = "../assets/icons/switch_down_black.svg"
+            rowSort(columnIndex + 1, "A-Z");
+            document.cookie = "sortCookie=" + columnIndex + "1"
+    }
 }
 
 function rowSort(index, mode) {
@@ -104,24 +142,57 @@ function rowSort(index, mode) {
         sortByArray.push(names[i].innerHTML)
     }
     sortByArray.sort();
-    console.log(sortByArray)
     var rows = document.querySelectorAll(".table_row")
     var rowNode = document.querySelector(".table_row_wrapper")
-    for (i = rows.length - 1; i >= 0; i--) {
-        rowNode.insertBefore(rows[indexFinder(sortByArray[i])], rowNode.firstElementChild)
-        rows = document.querySelectorAll(".table_row")
+    switch (mode) {
+        case "A-Z":
+            for (i = rows.length - 1; i > 0; i--) {
+                rowNode.insertBefore(rows[indexFinder(sortByArray[i], index, mode)], rowNode.firstElementChild)
+                rows = document.querySelectorAll(".table_row")
+            }
+            break;
+        case "Z-A":
+            for (i = 0; i < rows.length; i++) {
+                rowNode.insertBefore(rows[indexFinder(sortByArray[i], index, mode)], rowNode.firstElementChild)
+                rows = document.querySelectorAll(".table_row")
+            }
+            break;
+        default:
+            for (i = rows.length - 1; i >= 0; i--) {
+                rowNode.insertBefore(rows[indexFinder(defaultArray[i], 1, mode)], rowNode.firstElementChild)
+                rows = document.querySelectorAll(".table_row")
+            }
     }
-
 }
 //Gets the div index
-function indexFinder(nameSearchFor) {
-    console.log(i)
-    //console.log(nameSearchFor)
+function indexFinder(nameSearchFor, index, mode) {
     var rows = document.querySelectorAll(".table_row")
     for (j = 0; j < rows.length; j++) {
-        var currentName = rows[j].querySelector(".table_item:first-of-type p")
-        if (nameSearchFor === currentName.innerHTML && j > i) {
-            return j
+        var currentName = rows[j].querySelector(".table_item:nth-of-type(" + index + ") p")
+        if (nameSearchFor === currentName.innerHTML) {
+            switch (mode) {
+                case "A-Z":
+                    if (j < rows.length - 1 - i) {
+                        continue;
+                    }
+                    else {
+                        return j
+                    }
+                case "Z-A":
+                    if (j < i) {
+                        continue;
+                    }
+                    else {
+                        return j
+                    }
+                default:
+                    if (j < rows.length - 1 - i) {
+                        continue;
+                    }
+                    else {
+                        return j
+                    }
+            }
         }
     }
 }
