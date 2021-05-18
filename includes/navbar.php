009c1aed467ec5860e03da944bff266d52a6bdb7
kmarcell0 <kmarcell.kovi@gmail.com>
2021-05-18 23:23:29 +0200

    navbar redone with OOP

--- includes/navbar.php
+++ includes/navbar.php
@@ -21,7 +21,7 @@
 
         <p id="controls_text" class="nav_bar_title">CONTROLS</p>
         <!-- DT -->
-        <button type="button" <?php echo $assoc_array_functions['dt'] ?> >
+        <button class="nav_bar_item" onclick="toggle_dtDropdown(this)" type="button">
             <img src="../assets/icons/list_alt_black.svg">
             <p>DT</p>
             <img src="../assets/icons/arrow_drop_down_black.svg" id="dt_dropdown_icon" class="dropdown_icon">
@@ -29,32 +29,32 @@
 
         <!-- DT drop-down -->
         <div id="dt_dropdown_menu" class="dropdown_menu hidden">
-            <a <?php echo $assoc_array_functions['competitors'] ?>>Competitiors</a>
-            <a <?php echo $assoc_array_functions['pools'] ?>>Pools</a>
-            <a <?php echo $assoc_array_functions['temp_ranking'] ?>>Temporary Ranking</a>
-            <a <?php echo $assoc_array_functions['table'] ?>>Table</a>
-            <a <?php echo $assoc_array_functions['overview'] ?>>Overview</a>
+            <a href="<?php echo $navbar -> competitors -> href ?>" class="nav_bar_item <?php echo $navbar -> competitors -> class ?>">Competitiors</a>
+            <a href="<?php echo $navbar -> pools -> href ?>" class="nav_bar_item <?php echo $navbar -> pools -> class ?>">Pools</a>
+            <a href="<?php echo $navbar -> temporary_ranking -> href ?>" class="nav_bar_item <?php echo $navbar -> temporary_ranking -> class ?>">Temporary Ranking</a>
+            <a href="<?php echo $navbar -> table -> href ?>" class="nav_bar_item <?php echo $navbar -> table -> class ?>">Table</a>
+            <a href="<?php echo $navbar -> overview -> href ?>" class="nav_bar_item <?php echo $navbar -> overview -> class ?>">Overview</a>
         </div>
 
         <!-- callroom -->
-        <a <?php echo $assoc_array_functions['call_room'] ?>>
+        <a href="<?php echo $navbar -> call_room -> href ?>" class="nav_bar_item <?php echo $navbar -> call_room -> class ?>">
             <img src="../assets/icons/account_tree_black.svg">
             <p>Call Room</p>
         </a>
 
         <!-- registration -->
-        <a <?php echo $assoc_array_functions['registration'] ?>>
+        <a href="<?php echo $navbar -> registration -> href ?>" class="nav_bar_item <?php echo $navbar -> registration -> class ?>">
             <img src="../assets/icons/how_to_reg_black.svg">
             <p>Registration</p>
         </a>
 
         <!-- weapon control -->
-        <a <?php echo $assoc_array_functions['weapon_control'] ?>>
+        <a href="<?php echo $navbar -> weapon_control -> href ?>" class="nav_bar_item <?php echo $navbar -> weapon_control -> class ?>">
             <img src="../assets/icons/weapon_control_black.svg">
             <p>Weapon Control</p>
         </a>
 
-        <a <?php echo $assoc_array_functions['announcements'] ?>>
+        <a href="<?php echo $navbar -> announcements -> href ?>" class="nav_bar_item <?php echo $navbar -> announcements -> class ?>">
             <img src="../assets/icons/new_releases_black.svg">
             <p>Announcements</p>
         </a>
@@ -62,7 +62,7 @@
         <p id="setup_text" class="nav_bar_title">SETUP</p>
 
         <!-- general -->
-        <button type="button" <?php echo $assoc_array_functions['general'] ?>>
+        <button type="button"class="nav_bar_item" onclick="toggle_dtDropdown(this)">
             <img src="../assets/icons/widgets_black.svg">
             <p>General</p>
             <img src="../assets/icons/arrow_drop_down_black.svg" id="general_dropdown_icon" class="dropdown_icon">
@@ -70,13 +70,13 @@
 
         <!-- general drop-down -->
         <div id="general_dropdown_menu" class="dropdown_menu hidden">
-            <a <?php echo $assoc_array_functions['basic_info'] ?>>Basic Information</a>
-            <a <?php echo $assoc_array_functions['info_for_fencers'] ?>>Information for fencers</a></li>
-            <a <?php echo $assoc_array_functions['invitation'] ?>>Invitation</a>
+            <a href="<?php echo $navbar -> basic_information -> href ?>" class="nav_bar_item <?php echo $navbar -> basic_information -> class ?>">Basic Information</a>
+            <a href="<?php echo $navbar -> information_for_fencers -> href ?>" class="nav_bar_item <?php echo $navbar -> information_for_fencers -> class ?>">Information for fencers</a></li>
+            <a href="<?php echo $navbar -> invitation -> href ?>" class="nav_bar_item <?php echo $navbar -> invitation -> class ?>">Invitation</a>
         </div>
 
         <!-- technical -->
-        <button type="button" <?php echo $assoc_array_functions['technical'] ?>>
+        <button type="button"class="nav_bar_item" onclick="toggle_dtDropdown(this)">
             <img src="../assets/icons/home_repair_service_black.svg">
             <p>Technical</p>
             <img src="../assets/icons/arrow_drop_down_black.svg" id="technical_dropdown_icon" class="dropdown_icon">
@@ -84,20 +84,20 @@
 
         <!-- technical drop-down -->
         <div id="technical_dropdown_menu" class="dropdown_menu hidden">
-            <a <?php echo $assoc_array_functions['technicians'] ?>>Technicians</a>
-            <a <?php echo $assoc_array_functions['referees'] ?>>Referees</a>
-            <a <?php echo $assoc_array_functions['pistes'] ?>>Pistes</a>
-            <a <?php echo $assoc_array_functions['formula'] ?>>Formula</a>
+            <a href="<?php echo $navbar -> technicians -> href ?>" class="nav_bar_item <?php echo $navbar -> technicians -> class ?>">Technicians</a>
+            <a href="<?php echo $navbar -> referees -> href ?>" class="nav_bar_item <?php echo $navbar -> referees -> class ?>">Referees</a>
+            <a href="<?php echo $navbar -> pistes -> href ?>" class="nav_bar_item <?php echo $navbar -> pistes -> class ?>">Pistes</a>
+            <a href="<?php echo $navbar -> formula -> href ?>" class="nav_bar_item <?php echo $navbar -> formula -> class ?>">Formula</a>
         </div>
 
         <!-- ranking  -->
-        <a <?php echo $assoc_array_functions['ranking'] ?>>
+        <a href="<?php echo $navbar -> ranking -> href ?>" class="nav_bar_item <?php echo $navbar -> ranking -> class ?>">
             <img src="../assets/icons/leaderboard_black.svg">
             <p>Ranking</p>
         </a>
 
         <!-- manage ebtries -->
-        <a <?php echo $assoc_array_functions['manage_entries'] ?>>
+        <a href="<?php echo $navbar -> manage_entries -> href ?>" class="nav_bar_item <?php echo $navbar -> manage_entries -> class ?>">
             <img src="../assets/icons/all_inbox_black.svg">
             <p>Manage Entries</p>
         </a>
--- includes/navbar_btn_checker.php
+++ includes/navbar_btn_checker.php
@@ -1,108 +1,243 @@
-
 <?php
-    $query = "SELECT * FROM competitions WHERE comp_id = '$comp_id'";
-    $check_comp_query = mysqli_query($connection, $query);
 
-    if($row = mysqli_fetch_assoc($check_comp_query)){
+    class navbar {
 
-        $comp_status = $row["comp_status"];
-        $comp_id = $row["comp_id"];
-        $wc_type = $row['comp_wc_type'];
+        function __construct($connection, $comp_id) {
+            $array_names = ['overview','table','temporary_ranking','pools','competitors','call_room','registration','weapon_control','announcements','basic_information','information_for_fencers','invitation','technicians','referees','pistes','formula','ranking','manage_entries'];
 
-    }
-
-    define("NORMAL_CLASS", ' class="nav_bar_item" ');
-    define("DISABLED_CLASS", ' class="nav_bar_item disabled" ');
-
-    if ($wc_type == 1) {
-        $wc_page = 'href="weapon_control_immediate.php?comp_id=' . $comp_id . '"';
-    } else {
-        $wc_page = 'href="weapon_control_administrated.php?comp_id=' . $comp_id . '"';
-    }
-
-
-    //check for existing pool
-    $qry_check_pools = "SELECT `fencers`, `matches` FROM `pools` WHERE `assoc_comp_id` = '$comp_id'";
-    if ($do_check_pools = mysqli_query($connection, $qry_check_pools)) {
-        if ($row = mysqli_fetch_assoc($do_check_pools)) {
-            $matches = $row['matches'];
-            $fencers = $row['fencers'];
-
-            if ($fencers == NULL) {
-                $href_pools = 'href="../php/pools_generate.php?comp_id=' . $comp_id .'"';
-            } else if ($matches == NULL) {
-                $href_pools = 'href="../php/pools_config.php?comp_id=' . $comp_id .'"';
-            } else {
-                $href_pools = 'href="../php/pools_view.php?comp_id=' . $comp_id .'"';
+            //get competition state
+            $qry_get_state = "SELECT comp_status FROM competitions WHERE comp_id = '$comp_id'";
+            $do_get_state = mysqli_query($connection, $qry_get_state);
+            if ($row = mysqli_fetch_assoc($do_get_state)) {
+                $state = $row['comp_status'];
             }
-        } else {
-            $href_pools = 'href="../php/pools_generate.php?comp_id=' . $comp_id .'"';
+
+            foreach ($array_names as $name) {
+                if ($name == 'weapon_control') {
+                    $button_obj = new wc_button($state, $comp_id, $connection);
+                } else if ($name == 'pools') {
+                    $button_obj = new pool_button($state, $comp_id, $connection);
+                } else if ($name == 'call_room') {
+                    $button_obj = new call_room_button($state, $comp_id, $connection);
+                } else {
+                    $button_obj = new button($name, $state, $comp_id, $connection);
+                }
+
+                $this -> {$name} = $button_obj;
+            }
         }
-    } else {
-        $href_pools = 'href="../php/pools_generate.php?comp_id=' . $comp_id .'"';
     }
 
+    class button {
+        public $href = "";
+        public $class = "";
+        protected $name;
+        protected $state;
+        protected $path_beg = "../php/";
+        protected $path_end;
+        protected $role;
+        protected $comp_id;
+        public $conn;
 
+        protected function setComp_id($comp_id) {
+            $this -> path_end = ".php?comp_id=" . $comp_id;
+            $this -> comp_id = $comp_id;
+        }
 
-    $assoc_array_functions = // creating assoc array of button names and onclicks, and hrefs
-    [
-        "dt" =>                 'onclick="toggle_dtDropdown(this)"',
-        "competitors" =>        'href="competitors.php?comp_id=' . $comp_id . '"',
-        "pools" =>              $href_pools,
-        "temp_ranking" =>       'href="temporary_ranking.php?comp_id=' . $comp_id . '"',
-        "table" =>              'href="table.php?comp_id=' . $comp_id . '"',
-        "overview" =>           'href="overview.php?comp_id=' . $comp_id .'"',
-        "call_room" =>          'href="call_room.php?comp_id=' . $comp_id . '"',
-        "registration" =>       'href="registration.php?comp_id=' . $comp_id . '"',
-        "weapon_control" =>     $wc_page,
-        "announcements" =>      'href="announcements.php?comp_id=' . $comp_id . '"',
-        "general" =>            'onclick="toggle_dtDropdown(this)"',
-        "basic_info" =>         'href="basic_information.php?comp_id=' . $comp_id . '"',
-        "info_for_fencers" =>   'href="information_for_fencers.php?comp_id=' . $comp_id . '"',
-        "invitation" =>         'href="invitation.php?comp_id=' . $comp_id . '"',
-        "technical" =>          'onclick="toggle_dtDropdown(this)"',
-        "technicians" =>        'href="technicians.php?comp_id=' . $comp_id . '"',
-        "referees" =>           'href="referees.php?comp_id=' . $comp_id . '"',
-        "pistes" =>             'href="pistes.php?comp_id=' . $comp_id . '"',
-        "formula" =>            'href="formula.php?comp_id=' . $comp_id . '"',
-        "ranking" =>            'href="choose_ranking.php?comp_id=' . $comp_id . '"',
-        "manage_entries" =>     'href="manage_entries.php?comp_id=' . $comp_id . '"',
-    ];
+        public function __construct($name, $state, $comp_id, $connection) {
+            $this -> conn = $connection;
+            $this -> state = $state;
+            $this -> name = $name;
+            $this -> setComp_id($comp_id);
 
-    foreach ($assoc_array_functions as $key => $value) {
-        $assoc_array_functions[$key] = $value . NORMAL_CLASS;
+            $this -> determineRole();
+        }
+
+        protected function determineRole() {//when there will be technician login needs to be redone!
+            //$this -> role = $_SESSION['role'];
+            $this -> role = 'organiser';
+            if ($this -> role == 'organiser') {
+                $this -> setByState();
+            } else {
+                $this -> setByTechnician();
+            }
+        }
+
+        protected function setByState() {
+            $this -> state = 2;
+            switch ($this -> state) {
+                case 1:
+                    $disabled_buttons = ["temporary_ranking", "competitors", "pools", "table", "overview", "manage_entries"];
+                break;
+                case 2:
+                    $disabled_buttons = ["basic_information", "information_for_fencers", "ranking"];
+                break;
+                case 3:
+                    $disabled_buttons = ["general", "basic_information", "info_for_fencers", "timetable", "invitation", "ranking", "manage_entries"];
+                break;
+            }
+            $this -> setHrefClass($disabled_buttons);
+        }
+
+        protected function setByTechnician() {
+            switch ($this -> role) {
+                case 1: //semi
+                    $disabled_buttons = ['call_room','registration','weapon_control','announcements','basic_information','information_for_fencers','invitation','technicians','referees','pistes','formula','ranking','manage_entries'];
+                break;
+                case 2: //DT
+                    $disabled_buttons = ['call_room','registration','weapon_control','announcements','basic_information','information_for_fencers','invitation','technicians','referees','pistes','formula','ranking','manage_entries'];
+                break;
+                case 3: // weapon control
+                    $disabled_buttons = ['overview','table','temporary_ranking','pools','competitors','call_room','registration','announcements','basic_information','information_for_fencers','invitation','technicians','referees','pistes','formula','ranking','manage_entries'];
+                break;
+                case 4: //registration
+                    $disabled_buttons = ['overview','table','temporary_ranking','pools','competitors','call_room','weapon_control','announcements','basic_information','information_for_fencers','invitation','technicians','referees','pistes','formula','ranking','manage_entries'];
+                break;
+
+                $this -> setHrefClass($disabled_buttons);
+            }
+        }
+
+        protected function setHrefClass($disabled_buttons) {
+            if (in_array($this -> name, $disabled_buttons)) {
+                //class & href if disabled
+                $this -> class = "disabled";
+            } else {
+                //class & href if active
+                $this -> href = $this -> path_beg . $this -> name . $this -> path_end;
+            }
+        }
     }
 
-    switch ($comp_status) {
+    class wc_button extends button {
 
-        case 1: /* scheduled */
+        public function __construct($state, $comp_id, $connection) {
+            $this -> name = "weapon_control";
+            $this -> state = $state;
+            $this -> conn = $connection;
+            $this -> setComp_id($comp_id);
 
-            $array_button_names_for_change = ["dt", "competitors", "pools", "table", "overview", "manage_entries"];
+            $this -> determineRole();
 
-            foreach ($array_button_names_for_change as $name) {
-                $assoc_array_functions[$name] = DISABLED_CLASS;
+            if ($this -> href != "") {
+                //get weapon_control
+                $comp_id = $this -> comp_id;
+                $qry_get_data = "SELECT `comp_wc_type` FROM `competitions` WHERE `comp_id` = '$comp_id'";
+                $do_get_data = mysqli_query( $this -> conn, $qry_get_data);
+
+                if ($row = mysqli_fetch_assoc($do_get_data)) {
+                    $wc_type = $row['comp_wc_type'];
+
+                    $this -> setWeaponControl($wc_type);
+                }
             }
-        break;
-        case 2: /* published */
+        }
 
-            $array_button_names_for_change = ["basic_info", "info_for_fencers", "ranking"];
-
-            foreach ($array_button_names_for_change as $name) {
-                $assoc_array_functions[$name] = DISABLED_CLASS;
+        protected function setWeaponControl($wc_type) {
+            switch ($wc_type) {
+                case 0://no wc
+                    $this -> class = 'hidden';
+                break;
+                case 1://immidiate
+                    $this -> href = $this -> path_beg . "weapon_control_immediate" . $this -> path_end;
+                break;
+                case 2://administrative
+                    $this -> href = $this -> path_beg . "weapon_control_administrated" . $this -> path_end;
+                break;
             }
-
-        break;
-        case 3: /* outgoing */
-
-            $array_button_names_for_change = ["general", "basic_info", "info_for_fencers", "timetable", "invitation", "ranking", "manage_entries"];
-
-            foreach ($array_button_names_for_change as $name) {
-                $assoc_array_functions[$name] = DISABLED_CLASS;
-            }
-
-        break;
-        default: /* error line */
-            echo 'ERROR: YOUR COMPETITION DOES NOT HAVE CORRECT $comp_status! $comp_status=' . $comp_status;
-        break;
+        }
     }
+
+    class pool_button extends button {
+
+        public function __construct($state, $comp_id, $connection) {
+            $this -> name = "pools";
+            $this -> state = $state;
+            $this -> conn = $connection;
+            $this -> setComp_id($comp_id);
+
+            $this -> determineRole();
+
+            if ($this -> href != "") {
+                $comp_id = $this -> comp_id;
+                $qry_get_pools = "SELECT matches FROM pools WHERE assoc_comp_id = '$comp_id'";
+                if ($do_get_pools = mysqli_query($this -> conn, $qry_get_pools)) {
+                    if ($row = mysqli_fetch_assoc($do_get_pools)) {
+                        $matches_string = $row['matches'];
+
+                        if ($matches_string == NULL) { //pools config;
+                            $pool_phase = 1;
+                        } else { //pools view;
+                            $pool_phase = 2;
+                        }
+                        //phase 3 a pool done
+                    }
+                } else { // pools generate;
+                    $pool_phase = 0;
+                }
+
+                $this -> setPoolPhase($pool_phase);
+            }
+        }
+
+        public function setPoolPhase($pool_phase) {
+            switch ($pool_phase) {
+                case 0://pools_generate
+                    $this -> href = $this -> path_beg . "pools_generate" . $this -> path_end;
+                    break;
+                case 1://pools_config
+                    $this -> href = $this -> path_beg . "pools_config" . $this -> path_end;
+                    break;
+                case 2://pools_view
+                    $this -> href = $this -> path_beg . "pools_view" . $this -> path_end;
+                    break;
+                case 3://pools done
+
+                break;
+            }
+        }
+    }
+
+    class call_room_button extends button {
+
+        public function __construct($state, $comp_id, $connection) {
+            $this -> name = "call_room";
+            $this -> state = $state;
+            $this -> conn = $connection;
+            $this -> setComp_id($comp_id);
+
+            $this -> determineRole();
+
+            $comp_id = $this -> comp_id;
+            $qry_get_call_room = "SELECT data FROM formulas WHERE assoc_comp_id = '$comp_id'";
+            $do_get_call_room = mysqli_query($this -> conn, $qry_get_call_room);
+
+            if ($row = mysqli_fetch_assoc($do_get_call_room)) {
+                $formula_string = $row['data'];
+                $formula_json = json_decode($formula_string);
+
+                $call_room = $formula_json -> callRoom;
+
+                $this -> setCallRoom($call_room);
+            }
+        }
+
+        protected function setCallRoom($call_room) {
+            if ($call_room) {
+                $this -> href = $this -> path_beg . 'call_room' . $this -> path_end;
+            } else {
+                $this -> clas = "hidden";
+            }
+        }
+    }
+
+    $navbar = new navbar($connection, $comp_id);
+
+
+    // foreach ($navbar as $name => $buttons) {
+    //     echo $name . "<br>";
+    //     echo $buttons -> href . "<br>";
+    //     echo $buttons -> class . "<br>";
+    //     echo "<br>";
+    // }
 ?>
