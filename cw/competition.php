098437e7ba9c9f810a95b688e2ac124da296d386
kmarcell0 <kmarcell.kovi@gmail.com>
2021-05-25 22:49:01 +0200

    competition half commit

--- cw/competition.php
+++ cw/competition.php
@@ -7,9 +7,9 @@
     if (file_exists("../uploads/$comp_id.png")) {
         $logo_path = "../uploads/$comp_id.png";
     }
-    $qry_get_comp_data = "SELECT * FROM `competitors` WHERE `comp_id` = '$comp_id'";ű
+    $qry_get_comp_data = "SELECT * FROM `competitors` WHERE `comp_id` = '$comp_id'";
     $do_get_comp_data = mysqli_query($connection, $qry_get_comp_data);
-    if ($row = mysqli_query()) {
+    if ($row = mysqli_fetch_assoc($do_get_comp_data)) {
 
     }
 
