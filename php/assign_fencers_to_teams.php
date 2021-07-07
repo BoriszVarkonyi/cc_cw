b27d29e2ff3f31b6bbd54b155ca214efebbbc881
BoriszVarkonyi <borisz.varkonyi@gmail.com>
2021-07-07 10:45:31 +0200

    Team assign page modifications

--- php/assign_fencers_to_teams.php
+++ php/assign_fencers_to_teams.php
@@ -3,8 +3,67 @@
 <?php ob_start(); ?>
 <?php checkComp($connection); ?>
 
+<?php
+
+$qry_check_row = "SELECT data FROM competitors WHERE assoc_comp_id = '$comp_id'";
+$do_check_row = mysqli_query($connection, $qry_check_row);
+if ($row = mysqli_fetch_assoc($do_check_row)) {
+    $json_string = $row['data'];
+    $json_table = json_decode($json_string);
+}
+
+$qry_check_row = "SELECT data FROM teams WHERE assoc_comp_id = '$comp_id'";
+$do_check_row = mysqli_query($connection, $qry_check_row);
+if ($row = mysqli_fetch_assoc($do_check_row)) {
+    $json_string = $row['data'];
+    $json_teams = json_decode($json_string);
+}
+
+print_r($_POST);
+
+if (isset($_POST["assign_auto_submit"])) {
+
+    if ($_POST["role"] == 1) {
+
+        echo "response";
+
+        foreach ($json_teams as $team_name => $team) {
+            foreach ($json_table as $fencer) {
+                if (count($team->tireurs) >= 4) {
+                    break;
+                } elseif ($fencer->nation == $team->nation) {
+                    array_push($json_teams->$team_name->tireurs, $fencer);
+                }
+            }
+        }
+    }
+    if ($_POST["role"] == 2) {
+
+        echo "response";
+
+        foreach ($json_teams as $team_name => $team) {
+            foreach ($json_table as $fencer) {
+                if (count($team->tireurs) >= 4) {
+                    break;
+                } elseif ($fencer->club == $team->id) {
+                    array_push($json_teams->$team_name->tireurs, $fencer);
+                }
+            }
+        }
+    }
+
+    $json_string = json_encode($json_teams, JSON_UNESCAPED_UNICODE);
+    echo $qry_update = "UPDATE teams SET data = '$json_string' WHERE assoc_comp_id = '$comp_id'";
+    $qry_update_do = mysqli_query($connection, $qry_update);
+
+    echo mysqli_error($connection);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -13,6 +72,7 @@
     <link rel="stylesheet" href="../css/basestyle.min.css">
     <link rel="stylesheet" href="../css/mainstyle.min.css">
 </head>
+
 <body>
     <!-- header -->
     <div id="content_wrapper">
@@ -29,15 +89,15 @@
 
                     <a class="stripe_button bold" href="teams.php?comp_id=<?php echo $comp_id ?>">
                         <p>Go back to Teams</p>
-                        <img src="../assets/icons/arrow_back_ios_black.svg"/>
+                        <img src="../assets/icons/arrow_back_ios_black.svg" />
                     </a>
                     <button class="stripe_button bold" onclick="toggleAssignAutoPanel()">
                         <p>Assign Fencers Automatically</p>
-                        <img src="../assets/icons/list_alt_black.svg"/>
+                        <img src="../assets/icons/list_alt_black.svg" />
                     </button>
                     <button class="stripe_button primary" type="submit" form="save_team_assignments">
                         <p>Save</p>
-                        <img src="../assets/icons/save_black.svg"/>
+                        <img src="../assets/icons/save_black.svg" />
                     </button>
                 </div>
 
@@ -49,9 +109,9 @@
 
                         <label for="">ASSIGN AUTOMATICALLY BY</label>
                         <div class="option_container">
-                            <input type="radio" class="option_button" name="role" id="a" value="1"/>
+                            <input type="radio" class="option_button" name="role" id="a" value="1" />
                             <label for="a">Nation</label>
-                            <input type="radio" class="option_button" name="role" id="b" value="2"/>
+                            <input type="radio" class="option_button" name="role" id="b" value="2" />
                             <label for="b">Club</label>
                         </div>
                         <button type="submit" name="assign_auto_submit" class="panel_submit">Assign</button>
@@ -86,37 +146,27 @@
                             </tr>
                         </thead>
                         <tbody>
-                        <?php  ?>
-                            <tr>
-                                <td>
-                                    <p>Ez a neve</p>
-                                </td>
-                                <td>
-                                    <p>Ez nem</p>
-                                </td>
-                                <td>
-                                    <p>Ez igen ?</p>
-                                </td>
-                                <td class="square">
-                                    <input type="checkbox" name="emberek" id="ember1">
-                                    <label for="ember1"></label>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <p>Ez a neve</p>
-                                </td>
-                                <td>
-                                    <p>Ez nem</p>
-                                </td>
-                                <td>
-                                    <p>Ez igen ?</p>
-                                </td>
-                                <td class="square">
-                                    <input type="checkbox" name="emberek" id="ember2">
-                                    <label for="ember2"></label>
-                                </td>
-                            </tr>
+                            <?php
+
+
+                            foreach ($json_table as $json_obj) {
+                            ?>
+                                <tr id="<?php echo $json_obj->id ?>">
+                                    <td>
+                                        <p><?php echo $json_obj->prenom . " " . $json_obj->nom ?></p>
+                                    </td>
+                                    <td>
+                                        <p><?php echo $json_obj->nation ?></p>
+                                    </td>
+                                    <td>
+                                        <p><?php echo $json_obj->club ?></p>
+                                    </td>
+                                    <td class="square">
+                                        <input type="checkbox" name="emberek" id="<?php echo $json_obj->id ?>">
+                                        <label for="<?php echo $json_obj->id ?>"></label>
+                                    </td>
+                                </tr>
+                            <?php } ?>
                         </tbody>
                     </table>
                 </div>
@@ -131,31 +181,22 @@
                         </div>
                     </div>
                     <div class="splitscreen_section">
-                        <div class="splitscreen_select selected">
-                            <p>Team 1</p>
-                            <div class="red">
-                                <p>5</p>
-                                <img src="../assets/icons/person_black.svg">
+                        <?php
+
+                        foreach ($json_teams as $team_name => $team) {
+                        ?>
+                            <div class="splitscreen_select selected">
+                                <p><?php echo $team_name ?></p>
+                                <div class="red">
+                                    <p><?php echo count($team->tireurs) ?></p>
+                                    <img src="../assets/icons/person_black.svg">
+                                </div>
                             </div>
-                        </div>
-                        <div class="splitscreen_select">
-                            <p>Team 1</p>
-                            <div class="gray">
-                                <p>5</p>
-                                <img src="../assets/icons/person_black.svg">
-                            </div>
-                        </div>
-                        <div class="splitscreen_select">
-                            <p>Team 1</p>
-                            <div class="green">
-                                <p>5</p>
-                                <img src="../assets/icons/person_black.svg">
-                            </div>
-                        </div>
+                        <?php } ?>
                     </div>
                 </div>
             </div>
-        </div>
+    </div>
     </main>
     <script src="../js/cookie_monster.js"></script>
     <script src="../js/main.js"></script>
@@ -165,4 +206,5 @@
     <script src="../js/list_search.js"></script>
     <script src="../js/assign_fencers_to_teams.js"></script>
 </body>
+
 </html>
