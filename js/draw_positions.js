c3fc5335fd35b90e32c98b7ad8e8cdc0137cedd2
MEEP0321 <polyak.attila56@gmail.com>
2021-11-14 22:39:01 +0100

    team tale

--- js/draw_positions.js
+++ js/draw_positions.js
@@ -9,10 +9,11 @@ function drawPositions() {
     var teamInput = document.querySelector(".positions_wrapper input")
     var inputInJSON = JSON.parse(teamInput.value)
     var wrapperInput = document.querySelector(".positions_wrapper input")
-
+    var positionsGrid = document.querySelectorAll(".positions_grid")
 
 
     for (i = 0; i < Object.keys(inputInJSON[0]).length; i++) {
+        var gridDivs = positionsGrid[i].querySelectorAll("div")
         if (Object.keys(inputInJSON[0][Object.keys(inputInJSON[0])[i]])[0] == "" || Object.keys(inputInJSON[0][Object.keys(inputInJSON[0])[i]])[1] == "") {
             continue;
         }
@@ -20,9 +21,13 @@ function drawPositions() {
         inputInJSON[0][Object.keys(inputInJSON[0])[i]][Object.keys(inputInJSON[0][Object.keys(inputInJSON[0])[i]])[0]] = randomNumber
         if (randomNumber == 1) {
             inputInJSON[0][Object.keys(inputInJSON[0])[i]][Object.keys(inputInJSON[0][Object.keys(inputInJSON[0])[i]])[1]] = 0
+            gridDivs[3].innerHTML = "1-3"
+            gridDivs[1].innerHTML = "4-6"
         }
         else {
             inputInJSON[0][Object.keys(inputInJSON[0])[i]][Object.keys(inputInJSON[0][Object.keys(inputInJSON[0])[i]])[1]] = 1
+            gridDivs[1].innerHTML = "1-3"
+            gridDivs[3].innerHTML = "4-6"
         }
     }
 
