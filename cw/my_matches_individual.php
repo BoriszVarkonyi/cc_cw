4ebb45cc860612b86cb763bd1992bb9c7dc1610d
WolframKristof <wolfram.kristof@gmail.com>
2021-11-23 21:29:57 +0100

    Cw competititon changes for CV Fencer

---
+++ cw/my_matches_individual.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Finished competitions</title>
+    <link rel="stylesheet" href="../css/basestyle.min.css">
+    <link rel="stylesheet" href="../css/cw_mainstyle.min.css">
+</head>
+<body class="finished_competitions">
+    <?php include "cw_header.php"; ?>
+    <main>
+        <div id="content">
+            <div id="title_stripe">
+                <p class="stripe_title">My Matches</p>
+            </div>
+            <div id="content_wrapper">
+                <form method="POST" id="browsing_bar">
+
+                    <input name="submit_search" type="submit" value="Search">
+                </form>
+
+            </div>
+        </div>
+    </main>
+    <?php include "cw_footer.php"; ?>
+    <script src="../js/cookie_monster.js"></script>
+    <script src="../js/cw_bookmark_competition.js"></script>
+    <script src="../js/cw_main.js"></script>
+    <script src="../js/list.js"></script>
+    <script src="../js/competitions.js"></script>
+    <script src="../js/search.js"></script>
+</body>
+</html>
