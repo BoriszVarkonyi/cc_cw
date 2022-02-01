e71d0d60015647e846bf1862b3367604df326c17
tothzalan <toth.zalan03@gmail.com>
2022-02-01 21:53:48 +0100

    CW: weapon control kinda working

--- cw/weapon_control_report.php
+++ cw/weapon_control_report.php
@@ -1,5 +1,5 @@
-<?php
-    include "db.php";
+<?php 
+include "db.php";
 
     $comp_id = filter_input(INPUT_GET, "comp_id");
 
@@ -13,21 +13,62 @@
 
 
     $weapon_control_set = false;
+    $checked_out = false;
     $weapons = array();
 
     if($do_get_weapon_control) {
         $json_data = mysqli_fetch_assoc($do_get_weapon_control)["data"];
         $data = json_decode($json_data);
-        var_dump($data);
         if(property_exists($data, $_SESSION['fencer_id'])) {
             $fencer_id = $_SESSION['fencer_id'];
-            var_dump($data[$fencer_id]);
+            var_dump($data->$fencer_id);
+            $checked_out = $data->$fencer_id->checked_out;
             $weapon_control_set = true;
             $weapons = $data->{$fencer_id}->array_of_issues;
         }
     } else {
         echo mysqli_error($connection);
     }
+
+    //copied from cw/fencers_weapon_control.php
+    $array_issues = array(
+        "FIE mark on blade",
+        "Arm gap and weight",
+        "Arm lenght",
+        "Blade lenght",
+        "Grip lenght",
+        "Form and depth of the guard",
+        "Guard oxydation/ deformation",
+        "Excentricity of the blade",
+        "Blade flexibility",
+        "Curve on the blade",
+        "Foucault current device",
+        "point and arm size",
+        "spring of the point",
+        "total travel of the point",
+        "residual travel of the point",
+        "isolation of the point",
+        "resistance of the arm",
+        "length/ condition of body/ mask wire",
+        "resistance of body/ mask wire",
+        "mask: FIE mark",
+        "mask: condition and insulation",
+        "mask: resistance (sabre, foil)",
+        "metallic jacket condition",
+        "metallic jacket resistance",
+        "sabre glove/ overlay condition",
+        "sabre glove/ overlay resistance",
+        "glove condition",
+        "jacket condition",
+        "breeches condition",
+        "under-plastron condition",
+        "foil chest protector",
+        "socks",
+        "incorrect name printing",
+        "incorrect national logo",
+        "commercial",
+        "other items",
+    );
 ?>
 
 <!DOCTYPE html>
@@ -50,7 +91,8 @@
             <div id="content_wrapper">
                 <div id="weapon_control_info" class="red">
                     <p id="fencer_name"><?php echo $_SESSION['fencer_name'] ?>'s Weapon Control</p>
-                    <p id="wc_status">Status: <?php if($weapon_control_set) echo "Set"; else echo "Unset" ?></p>
+                    <p id="wc_status">Status: <?php if($weapon_control_set) echo "Set"; else echo "Unset"; ?></p>
+                    <p><?php if($checked_out) echo "Checked Out"; else echo "Not checked out"; ?></p>
                 </div>
 
                 <!-- only if administrated weapo control -->
@@ -63,14 +105,22 @@
                         </tr>
                     </thead>
                     <tbody class="alt">
-                        <tr>
-                            <td>
-                                <p>EQ</p>
-                            </td>
-                            <td>
-                                <p>quantritxy</p>
-                            </td>
-                        </tr>
+                        <?php if(property_exists($data, $_SESSION['fencer_id'])): ?>
+                            <?php foreach($data->$fencer_id->equipment as $key => $quantity): ?>
+                                <tr>
+                                    <td>
+                                        <p><?php echo $array_issues[$key]; ?></p>
+                                    </td>
+                                    <td>
+                                        <p><?php echo $quantity; ?></p>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
+                        <?php else: ?>
+                            <tr>
+                                <td colspan="2">Nothing found</td>
+                            </tr>
+                        <?php endif ?>
                     </tbody>
                 </table>
 
