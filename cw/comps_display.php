22d0fdb455183938f4064ace0edba36b510acb65
tothzalan <toth.zalan03@gmail.com>
2022-02-09 21:28:58 +0100

    CW: comps forms update

--- cw/comps_display.php
+++ cw/comps_display.php
@@ -27,6 +27,7 @@ $query = "SELECT * FROM competitions " . $WHERE_CLAUSE;
 $select_all_comps = mysqli_query($connection, $query);
 
 $empty = true;
+if($select_all_comps) {
 while ($row = mysqli_fetch_assoc($select_all_comps)){
     $empty = false;
     $comp_name = $row['comp_name'];
@@ -88,7 +89,9 @@ while ($row = mysqli_fetch_assoc($select_all_comps)){
                 }
             ?>
         </tr>
-<?php } ?>
+<?php
+    } }
+?>
         <?php
             if ($empty) {
         ?>
