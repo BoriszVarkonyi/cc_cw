70d04b8994d4c6ade480282518153564f59cade4
kmarcell0 <kmarcell.kovi@gmail.com>
2022-02-20 20:14:13 +0100

    fencers_weapon_control immidiate done

--- cc/fencers_weapon_control.php
+++ cc/fencers_weapon_control.php
@@ -76,21 +76,21 @@
     }
 
     //get fencers existing wc
-    $qry_select = "SELECT notes FROM weapon_control WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id' LIMIT 1";
+    $qry_select = "SELECT issues_array FROM weapon_control WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id' LIMIT 1";
     $do_select = mysqli_query($connection, $qry_select);
-    if (mysqli_fetch_assoc($do_select)['notes'] === null) {
+
+    if (($array_string = mysqli_fetch_assoc($do_select)['issues_array']) === null) {
         $notes = "";
         $real_issues_array = [];
         foreach ($array_issues as $issue) {
             $real_issues_array[] = 0;
         }
     } else {
-        $notes = mysqli_fetch_assoc($do_select)['notes'];
-        $qry_select2 = "SELECT issues_array FROM weapon_control WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id' LIMIT 1";
+        $qry_select2 = "SELECT notes FROM weapon_control WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id' LIMIT 1";
         $do_select2 = mysqli_query($connection, $qry_select2);
         if ($row = mysqli_fetch_assoc($do_select2)) {
-            $string = $row['issues_array'];
-            $real_issues_array = json_decode($string);
+            $notes = $row['notes'];
+            $real_issues_array = json_decode($array_string);
         } else {
             echo "error: atika matika: " . mysqli_error($connection) . "<br>";
         }
@@ -108,26 +108,26 @@
                 $real_issues_array[$i] = 0;
             } else {
                 $real_issues_array[$i] = $_POST['issue_n_' . $i];
-                $empty = false;
             }
         }
 
-        if ($notes !== "") {
-            $empty = false;
+        $temps = json_encode($real_issues_array);
+
+        $qry_update = "UPDATE weapon_control SET notes = '$notes', issues_array = '$temps' WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id'";
+        if (!mysqli_query($connection, $qry_update)) {
+            echo "See you again! " . mysqli_error($connection);
+
+        } else {
+            header("Location: ../cc/$wc_page.php?comp_id=$comp_id");
+            //echo "St vitus dance";
         }
+    }
 
-        if (!$empty) {
-            $temps = json_encode($real_issues_array);
 
-            $qry_update = "UPDATE weapon_control SET notes = '$notes', issues_array = '$temps' WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id'";
-            if (!mysqli_query($connection, $qry_update)) {
-                echo "See you again! " . mysqli_error($connection);
-            } else {
-                echo "St vitus dance";
-            }
-
-        }
-        //header("Location: ../cc/$wc_page.php?comp_id=$comp_id");
+    if (isset($_POST['delete_wc'])) {
+        $qry_delete = "UPDATE weapon_control SET notes = null, issues_array = null WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id'";
+        $do_delete = mysqli_query($connection, $qry_delete);
+        header("Location: ../cc/$wc_page.php?comp_id=$comp_id");
     }
 
 ?>
@@ -157,7 +157,7 @@
                     </a>
                     <button name="delete_wc" class="stripe_button red" type="submit" form="fencers_weapon_control_wrapper">
                         <p>Delete Weapon Control</p>
-                        <img src="../assets/icons/save_black.svg"/>
+                        <img src="../assets/icons/delete_black.svg"/>
                     </button>
                     <button name="submit_wc" class="stripe_button primary" type="submit" form="fencers_weapon_control_wrapper">
                         <p>Save Weapon Control</p>
@@ -196,8 +196,8 @@
 
                             <?php
                                 foreach ($array_issues as $issue_id => $issue) {
-                                if (isset($array_of_real_issues[$issue_id]) && $array_of_real_issues[$issue_id] != 0) {
-                                    $issue_numbers = $array_of_real_issues[$issue_id];
+                                if ($real_issues_array[$issue_id] != 0) {
+                                    $issue_numbers = $real_issues_array[$issue_id];
                                 } else {
                                     $issue_numbers = "";
                                 }
