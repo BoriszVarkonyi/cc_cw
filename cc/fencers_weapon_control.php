6d8aa46cfa09f8b291a76e5ee9432b6ef1c174bf
WolframKristof <wolfram.kristof@gmail.com>
2022-03-16 21:14:26 +0100

    wc buttons added for JS

--- cc/fencers_weapon_control.php
+++ cc/fencers_weapon_control.php
@@ -155,6 +155,9 @@
                                 <th>
                                     <p>QUANTITY</p>
                                 </th>
+                                <th class="wide_controls">
+                                    <p>CONTROLS</p>
+                                </th>
                             </tr>
                         </thead>
                         <tbody>
@@ -171,9 +174,10 @@
                             <tr>
                                 <td><p><?php echo $issue ?></p></td>
                                 <td><input value="<?php echo $issue_numbers?>" name="issue_n_<?php echo $issue_id ?>" type="number" placeholder="#"></td>
-                                <td>
-                                    <button onclick="addNumber(1)"><img src="../assets/icons/add_black.svg"></button>
-                                    <button onclick="addNumber(-1)"><img src="../assets/icons/close_black.svg"></button>
+                                <td class="wide_controls">
+                                    <button type="button" onclick="addNumber(this, 1)"><img src="../assets/icons/add_black.svg"></button>
+                                    <button type="button" onclick="addNumber(this, -1)"><img src="../assets/icons/close_black.svg"></button>
+                                    <button type="button" onclick="resetNumber(this)"><img src="../assets/icons/reset_black.svg"></button>
                                 </td>
                             </tr>
 
