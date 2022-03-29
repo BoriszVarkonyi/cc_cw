9f5c78113ec93dc923d4de509867ac20a8074197
kmarcell0 <kmarcell.kovi@gmail.com>
2022-03-29 22:14:21 +0200

    call_room_statistics done

--- cc/callroom_statistics.php
+++ cc/callroom_statistics.php
@@ -1,6 +1,6 @@
 <?php include "includes/header.php"; ?>
 <?php include "includes/db.php" ?>
-<?php include "includes/wc_issues_array.php"; ?>
+<?php include "includes/issues_array.php"; ?>
 <?php ob_start(); ?>
 <?php checkComp($connection); ?>
 <?php
@@ -16,6 +16,135 @@
 
     } else {
         echo "error:    " . mysqli_error($connection);
+    }
+
+
+    //check for wc type
+    $qry_check = "SELECT comp_wc_type FROM competitions WHERE comp_id = '$comp_id'";
+    $do_check = mysqli_query($connection, $qry_check);
+    if ($row = mysqli_fetch_assoc($do_check)) {
+        $wc_type = $row['comp_wc_type'];
+    }
+
+    //sort fencer ids by nation
+    $qry_get_compet_table = "SELECT data FROM competitors WHERE assoc_comp_id = '$comp_id'";
+    $do_get_compet_table = mysqli_query($connection, $qry_get_compet_table);
+    if ($row = mysqli_fetch_assoc($do_get_compet_table)) {
+        $string_table = $row['data'];
+        $competit_table = json_decode($string_table);
+    }
+
+    $sorted_teams_array = [];
+
+    for ($i = 0; $i < count($competit_table); $i++) {
+        if(isset($competit_table[$i]->$sort_by))
+            $fencers_nat = $competit_table[$i] -> $sort_by;
+        $fencers_id = $competit_table[$i] -> id;
+
+        if(isset($fencers_nat))
+            $sorted_teams_array[$fencers_nat][] = $fencers_id;
+    }
+
+
+    $qry_select_wc = "SELECT issues_array FROM call_room_wc WHERE assoc_comp_id = '$comp_id'";
+    $do_select_wc = mysqli_query($connection, $qry_select_wc);
+
+    $all_wcs_count= 0;
+    $all_issues_count = 0;
+    $submitted_wcs_count = 0;
+    $perfect_fencers_count = 0;
+    $imperfect_fencers_count = 0;
+    $sorted_issues_array = [];
+    while ($row = mysqli_fetch_assoc($do_select_wc)) {
+        //check fencer
+        $all_wcs_count++;
+        if ($row['issues_array'] !== null) {
+            $submitted_wcs_count++;
+            $real_issues_string = $row['issues_array'];
+            $real_issues_array = explode(',', $real_issues_string);
+            //loop through fencers issues
+            $has_issues = false;
+            foreach($real_issues_array as $item) {
+                $all_issues_count += $item;
+                if($has_issues == false && $item != 0) {
+                    $has_issues = true;
+                    $imperfect_fencers_count++;
+                }
+            }
+            if($has_issues == false) $perfect_fencers_count++;
+
+            for ($issues_id = 0; $issues_id < count($cr_array_issues); $issues_id++) {
+                $count_issue = $real_issues_array[$issues_id];
+                if ($count_issue > 0) {
+                    if (isset($sorted_issues_array[$issues_id])) {
+                        $sorted_issues_array[$issues_id] += $count_issue;
+                    } else {
+                        $sorted_issues_array[$issues_id] = $count_issue;
+                    }
+                }
+            }
+        }
+    }
+    //make sorted issues array
+    $assoc_sorted_issues_array = [];
+    foreach ($sorted_issues_array as $issue_id => $issue_value) {
+        if (isset($assoc_sorted_issues_array[$cr_array_issues[$issue_id]])) {
+            $assoc_sorted_issues_array[$cr_array_issues[$issue_id]] += $issue_value;
+        } else {
+            $assoc_sorted_issues_array[$cr_array_issues[$issue_id]] = $issue_value;
+        }
+    }
+    arsort($assoc_sorted_issues_array, 1);
+
+
+    //make sorted club nat
+    //print_r($sorted_teams_array);
+
+    $teams_sum_issues = [];
+    $teams_indi_issues = [];
+    $issue_num_teams = [];
+    $fencer_in_team = [];
+    foreach ($sorted_teams_array as $nation => $fencer_ids_array) {
+        $teams_sum_issues[$nation] = [];
+        $teams_indi_issues[$nation] = [];
+        $issue_num_teams[$nation] = 0;
+        $fencer_in_team[$nation] = 0;
+
+        foreach ($fencer_ids_array as $fencer_id) {
+            $fencer_in_team[$nation]++;
+            $fencers_sum_issues = 0;
+            $qry_select_fencer = "SELECT issues_array FROM call_room_wc WHERE assoc_comp_id = '$comp_id' AND fencer_id = '$fencer_id'";
+            $do_select_fencer = mysqli_query($connection, $qry_select_fencer);
+            if ($row = mysqli_fetch_assoc($do_select_fencer)) {
+                $issues_string = $row['issues_array'];
+                $real_issues_array = explode(',', $issues_string);
+                //loop through issues
+                if ($real_issues_array != null) {
+                    for ($i = 0; $i < count($real_issues_array); $i++) {
+                        $current_issues_value = $real_issues_array[$i];
+                        $current_issues_name = $cr_array_issues[$i];
+                        $fencers_sum_issues += $current_issues_value;
+                        $issue_num_teams[$nation] += $current_issues_value;
+                        if (isset($teams_sum_issues[$nation][$current_issues_name])) {
+                            //get for all fencers
+                            $teams_sum_issues[$nation][$current_issues_name] += $current_issues_value;
+                        } else {
+                            $teams_sum_issues[$nation][$current_issues_name] = $current_issues_value;
+                        }
+                    }
+                    //get fencers name
+                    if (($id_to_find = findObject($competit_table, $fencer_id, "id")) !== false) {
+                        $fencer_name = $competit_table[$id_to_find] -> prenom . " " . $competit_table[$id_to_find] -> nom;
+                    } else {
+                        echo "could not find fencer by id";
+                    }
+                    $teams_indi_issues[$nation][$fencer_name]["id"] = $fencer_id;
+                    $teams_indi_issues[$nation][$fencer_name]["issues"] = $fencers_sum_issues;
+                    arsort($teams_indi_issues[$nation]);
+                    arsort($teams_sum_issues[$nation], 1);
+                }
+            }
+        }
     }
 ?>
 <!DOCTYPE html>
@@ -62,22 +191,22 @@
                                 <a class="stat" href="callroom_immediate.php?comp_id=<?php echo $comp_id ?>">
                                     <img src="../assets/icons/weapon_control_black.svg">
                                     <p class="stat_title">Callrooms</p>
-                                    <p class="stat_number">52 / 4</p>
+                                    <p class="stat_number"><?php echo $submitted_wcs_count ?></p>
                                 </a>
                                 <a class="stat" href="callroom_statistics.php?comp_id=<?php echo $comp_id ?>">
                                     <img src="../assets/icons/report_problem_black.svg">
                                     <p class="stat_title">Issues Reported</p>
-                                    <p class="stat_number">14</p>
+                                    <p class="stat_number"><?php echo $all_issues_count ?></p>
                                 </a>
                                 <a class="stat" href="callroom_statistics.php?comp_id=<?php echo $comp_id ?>">
                                     <img src="../assets/icons/report_problem_black.svg">
                                     <p class="stat_title">Fencers without equipment issues</p>
-                                    <p class="stat_number">3</p>
+                                    <p class="stat_number"><?php echo $perfect_fencers_count ?></p>
                                 </a>
                                 <a class="stat" href="callroom_statistics.php?comp_id=<?php echo $comp_id ?>">
                                     <img src="../assets/icons/report_problem_black.svg">
                                     <p class="stat_title">Fencers with equipment issues</p>
-                                    <p class="stat_number">1</p>
+                                    <p class="stat_number"><?php echo $imperfect_fencers_count ?></p>
                                 </a>
                             </div>
                         </div>
@@ -170,25 +299,25 @@
                                 </thead>
                                 <tbody class="alt">
                                     <?php
-                                    /*
+
                                         $empty = true;
                                         foreach ($assoc_sorted_issues_array as $key => $value) {
                                         $empty = false;
-                                    */
+
                                     ?>
                                     <tr>
                                         <td>
-                                            <p><?php /* echo $key */ ?>issue name</p>
+                                            <p><?php echo $key ?></p>
                                         </td>
                                         <td>
-                                            <p><?php /* echo $value */ ?> no. of issues</p>
+                                            <p><?php echo $value ?></p>
                                         </td>
                                     </tr>
 
                                     <?php
-                                    /*
+
                                         }
-                                        if ($empty)  {*/?>
+                                        if ($empty)  {?>
 
                                     <tr>
                                         <td colspan="2">
@@ -197,7 +326,7 @@
                                     </tr>
 
 
-                                        <?php /*}*/
+                                        <?php }
                                     ?>
                                 </tbody>
                             </table>
@@ -212,22 +341,22 @@
                         <div class="print_stat">
                             <img src="../assets/icons/weapon_control_black.svg">
                             <p class="bold">Callrooms</p>
-                            <p>54 / 4</p>
+                            <p><?php echo $submitted_wcs_count ?></p>
                         </div>
                         <div class="print_stat">
                             <img src="../assets/icons/report_problem_black.svg">
                             <p class="bold">Issues Reported</p>
-                            <p>5</p>
+                            <p><?php echo $all_issues_count ?></p>
                         </div>
                         <div class="print_stat">
                             <img src="../assets/icons/report_problem_black.svg">
                             <p class="bold">Fencers without equipment issues</p>
-                            <p>1</p>
+                            <p><?php echo $perfect_fencers_count ?></p>
                         </div>
                         <div class="print_stat">
                             <img src="../assets/icons/report_problem_black.svg">
                             <p class="bold">Fencers with equipment issues</p>
-                            <p>1</p>
+                            <p><?php echo $imperfect_fencers_count ?></p>
                         </div>
 
                     </div>
