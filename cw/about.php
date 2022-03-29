f7af97ee5db5cb718a11aa57950ffa04c84ebdfc
tothzalan <toth.zalan03@gmail.com>
2022-03-29 22:06:30 +0200

    CW: change title I should look at the code before pushing

--- cw/about.php
+++ cw/about.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Videos</title>
+    <title>About us</title>
     <link rel="stylesheet" href="../css/basestyle.min.css">
     <link rel="stylesheet" href="../css/dv_mainstyle.min.css">
 </head>
