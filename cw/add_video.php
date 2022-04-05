f7671188d6ca8036079fd8a50e98c0f707d55fb8
tothzalan <toth.zalan03@gmail.com>
2022-04-05 21:30:39 +0200

    CW: video new input to specify comp_id

--- cw/add_video.php
+++ cw/add_video.php
@@ -6,18 +6,19 @@
     //create table
     $qry_table = "CREATE TABLE `ccdatabase`.`cw_videos` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `URL` VARCHAR(255) NOT NULL , `title` VARCHAR(255) NOT NULL , `comp_name` VARCHAR(255) NOT NULL , `prev` TEXT NOT NULL , `author` VARCHAR(255) NOT NULL , `Last_modified_by` VARCHAR(255) NOT NULL , `Date_of_creation` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , `Last_modified` DATE NOT NULL DEFAULT CURRENT_TIMESTAMP , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
     $do_table = mysqli_query($connection, $qry_table);
-    echo mysqli_error($connection);
+    //echo mysqli_error($connection);
 
 
     if (isset($_POST['submit'])) {
         $title = htmlspecialchars($_POST['title'], ENT_QUOTES);
         $url = htmlspecialchars($_POST['url'], ENT_QUOTES);
         $comp_name = htmlspecialchars($_POST['comp_name'], ENT_QUOTES);
+        $comp_id = htmlspecialchars($_POST['comp_id'], ENT_QUOTES) ?? null;
         $prev_text = htmlspecialchars($_POST['prev_text'], ENT_QUOTES);
 
         if ($title != "" && $url != "" && $comp_name != "" && $prev_text != "") {
             $date = date("y-m-d");
-            $qry_insert = "INSERT INTO cw_videos (URL, title, comp_name, prev, author, Last_modified_by, Date_of_creation) VALUES ('$url', '$title', '$comp_name', '$prev_text', '$username', '$username', '$date')";
+            $qry_insert = "INSERT INTO cw_videos (URL, title, comp_name, prev, author, comp_id, Last_modified_by, Date_of_creation) VALUES ('$url', '$title', '$comp_name', '$prev_text', '$username', '$comp_id', '$username', '$date')";
             $do_insert = mysqli_query($connection, $qry_insert);
             echo mysqli_error($connection);
         }
@@ -37,10 +38,16 @@
     <p><h1>Add video to videos as <?php echo $username ?></h1><p>
     <div class="basic_panel">
         <form method="POST">
-            <input name="url" type="text" placeholder="Put your youtube link here!"> <br>
-            <input name="title" type="text" placeholder="Put your video title here"><br>
-            <input name="comp_name" type="text" placeholder="Type the name of competition here!"><br>
-            <input name="prev_text" type="text" placeholder="Type your previes text here!"><br>
+            <label for="url">Youtube link</label>
+            <input id="url" name="url" type="text" placeholder="Put your youtube link here!"> <br>
+            <label for="title">Video title</label>
+            <input id="title" name="title" type="text" placeholder="Put your video title here"><br>
+            <label for="comp_name">Competition name</label>
+            <input id="comp_name" name="comp_name" type="text" placeholder="Type the name of competition here!"><br>
+            <label for="comp_id">Competition id</label>
+            <input id="comp_id" name="comp_id" type="text" placeholder="Type the id of the comp here"><br>
+            <label for="prev_text">Preview text</label>
+            <input id="prev_text" name="prev_text" type="text" placeholder="Type your preview text here!"><br>
             <input name="submit" type="submit" placeholder="Submit">
         </form>
     </div>
