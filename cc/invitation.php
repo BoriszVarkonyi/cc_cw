c072046971854a11e637d170c5aa8f80343eabe4
kmarcell0 <kmarcell.kovi@gmail.com>
2022-04-05 21:36:31 +0200

    invitations.php now displays information needs printing and some touchups

--- cc/invitation.php
+++ cc/invitation.php
@@ -3,40 +3,38 @@
 <?php checkComp($connection); ?>
 
 <?php
-$feedback = array(
-    "delete" => "",
-    "update" => "",
-    "getnumrows" => "",
-    "getdata" => "",
-    "insert_info" => "",
-    "ttest" => "",
-    "create" => ""
-);
+
+    $qry_make_table = "CREATE TABLE `ccdatabase`.`plus_info` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `assoc_comp_id` INT(11) NOT NULL , `title` VARCHAR(255) NOT NULL , `body` TEXT NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    $make_table = mysqli_query($connection, $qry_make_table);
+
 
 $kuka_disable = "panel_button";
 
-//array of colums I need from database
-$array_getdata = array("comp_name", "comp_status","comp_sex", "comp_weapon", "comp_equipment", "comp_info", "comp_wc_type");
-
 //connecting to database
-$qry_getdata = "SELECT * FROM competitions WHERE comp_id = $comp_id";
+$qry_getdata = "SELECT * FROM competitions WHERE comp_id = '$comp_id'";
 $getdata_do = mysqli_query($connection, $qry_getdata);
 
 if ($row = mysqli_fetch_assoc($getdata_do)) {
 
-    //Putting the data from database into an assoc array with $array_getdata[] indexes
-    for ($i = 0; $i <  count($array_getdata); ++$i) {
-
-        if ($i == 0) {
-            $assoc_array_data = [$array_getdata[$i] => $row[$array_getdata[$i]]];
-        } else {
-            $assoc_array_data[$array_getdata[$i]] = $row[$array_getdata[$i]];
-        }
-    }
-} else { //error branch
-    $feedback['getdata'] = "ERROR: " . mysqli_error($connection);
+    $comp_name = $row['comp_name'];
+    $comp_status = $row['comp_status'];
+    $comp_sex = $row['comp_sex'];
+    $comp_weapon = $row['comp_weapon'];
+    $comp_equipment = $row['comp_equipment'];
+    $comp_info = $row['comp_info'];
+    $comp_wc_type = $row['comp_wc_type'];
 }
 
+//get basic_info
+$qry_get_info = "SELECT data FROM basic_info WHERE assoc_comp_id = '$comp_id'";
+$do_get_info = mysqli_query($connection, $qry_get_info);
+if ($row = mysqli_fetch_assoc($do_get_info)) {
+    $string = $row['data'];
+
+} else {
+    $string = '{"host_country":"&#8709","city_street":"&#8709","zip_code":"&#8709","entry_fee":"&#8709","starting_date":"&#8709","ending_date":"&#8709","end_of_pre_reg":"&#8709"}';
+}
+$basic_info = json_decode($string);
 //get logo image
 if (file_exists("../uploads/" . $comp_id . ".png")) {
 
@@ -52,74 +50,47 @@ if (file_exists("../uploads/" . $comp_id . ".png")) {
 
 //update plusinfo table with new title from form
 if (isset($_POST['info_submit']) && 0 < strlen($_POST['info_title'])) {
+    $qry_make_table = "CREATE TABLE `ccdatabase`.`plus_info` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `assoc_comp_id` INT(11) NOT NULL , `title` VARCHAR(255) NOT NULL , `body` TEXT NULL DEFAULT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    $make_table = mysqli_query($connection, $qry_make_table);
 
-    $check_d_table_qry = "SHOW TABLES LIKE 'info_$comp_id'";
-    $check_d_table_do = mysqli_query($connection, $check_d_table_qry);
-    $row = mysqli_num_rows($check_d_table_do);
+    $title = mysqli_real_escape_string($connection, $_POST['info_title']);
 
-    if ($row == 0) {
-
-        $create_table_qry = "CREATE TABLE `info_$comp_id` ( `id` INT(11) NOT NULL AUTO_INCREMENT , `info_title` VARCHAR(255) NOT NULL , `info_body` TEXT NOT NULL, PRIMARY KEY (id)) ENGINE = InnoDB";
-
-
-        if (mysqli_query($connection, $create_table_qry)) {
-            $feedback['create'] = "Minden fasza!";
-        } else {
-            $feedback['create'] = "ERROR " . mysqli_error($connection);
-        }
-    } else;
-
-    $text_title = $_POST['info_title'];
-
-    //test for duplicate rows
-    $dupli_rows_qry = "SELECT * FROM info_$comp_id WHERE info_title = '$text_title'";
-    if ($dupli_rows_do = mysqli_query($connection, $dupli_rows_qry)) {
-
-        $numrows = mysqli_num_rows($dupli_rows_do);
+    //insert row
+    $qry_insert_new_row = "INSERT INTO plus_info (assoc_comp_id, title) VALUES ('$comp_id', '$title')";
+    if ($do_insert_new_row = mysqli_query($connection, $qry_insert_new_row)) {
+        header("Refresh: 0");
     } else {
-        $feedback['getnumrows'] = "ERROR: " . mysqli_error($connection);
-    }
-
-    if ($numrows == 0) {
-
-        $insert_info_qry = "INSERT INTO info_$comp_id (info_title) VALUE ('$text_title')";
-        $inser_indo_do = mysqli_query($connection, $insert_info_qry);
-
-        if ($inser_indo_do) {
-            $feedback['insert_info'] = "minden OK!";
-        } else { //error branch
-            $feedback['insert_info'] =  "ERROR:" . mysqli_error($connection);
-        }
-    } else {
-        $feedback['insert_info'] = "You already have info with the same title!";
+        echo mysqli_error($connection);
     }
 }
 
 
 //updateing info_body from text areas
 if (isset($_POST['submit_body'])) {
+    $body = mysqli_escape_string($connection, $_POST['text_body']);
+    $id = $_POST['entry_id'];
+    $title = "";
 
-    $new_info_body = $_POST['text_body'];
-    $change_title = $_POST['text_title_to_change'];
-
-    $update_qry = "UPDATE info_$comp_id SET info_body = '$new_info_body' WHERE info_title = '$change_title'";
-    if (mysqli_query($connection, $update_qry)) {
-        $feedback['update'] = "Minden ok!";
+    $qry_update_with_body = "UPDATE plus_info SET body = '$body' WHERE id = '$id'";
+    if ($do_update_with_body = mysqli_query($connection, $qry_update_with_body)) {
+        header("Refresh: 0");
     } else {
-        $feedback['update'] = "ERROR:" . mysqli_error($connection);
+        echo mysqli_error($connection);
     }
+
 }
 
 //deleteing info_$comp_id row
 if (isset($_POST['submit_delete'])) {
 
-    $change_title = $_POST['text_title_to_change'];
+    $id_to_del = $_POST['entry_id'];
 
-    $delete_qry = "DELETE FROM info_$comp_id WHERE info_title = '$change_title'";
-    if (mysqli_query($connection, $delete_qry)) {
-        $feedback['delete'] = "Minden ok delete!";
+    //drop row
+    $qry_delete = "DELETE FROM plus_info WHERE id='$id_to_del'";
+    if ($do_del = mysqli_query($connection, $qry_delete)) {
+        header("Refresh: 0");
     } else {
-        $feedback['delete'] = "ERROR:" . mysqli_error($connection);
+        echo mysqli_error($connection);
     }
 }
 
@@ -184,32 +155,29 @@ if (file_exists("../uploads/$comp_id.png")) {
                                 <div class="table_row_wrapper alt">
                                     <?php
 
-                                    //displaying plsu infos from db in table rows
-                                    $get_data_plusinfo_qry = "SELECT * FROM info_$comp_id";
-                                    $get_data_plusinfo_do = mysqli_query($connection, $get_data_plusinfo_qry);
-                                    if ($get_data_plusinfo_do !== FALSE) {
-                                        while ($row = mysqli_fetch_assoc($get_data_plusinfo_do)) {
+                                        $qry_display = "SELECT id, title, body FROM plus_info WHERE assoc_comp_id = '$comp_id'";
+                                        $do_display = mysqli_query($connection, $qry_display);
 
-                                            $info_title = $row['info_title'];
-                                            $info_body = $row['info_body'];
+                                        while ($row = mysqli_fetch_assoc($do_display)) {
                                     ?>
 
                                             <div class="entry">
                                                 <div class="table_row" onclick="toggleEntry(this)">
-                                                    <div class="table_item invitation"><?php echo $info_title ?></div>
+                                                    <!-- ezt át lehetne pakolni egy inputra es akkor tudna megvaltoztatni mostmar a titlet is az nber ##KRIS -->
+                                                    <div class="table_item invitation"><?php echo $row['title'] ?></div>
                                                 </div>
                                                 <form class="entry_panel collapsed" id="update" method="POST" action="../cc/invitation.php?comp_id=<?php echo $comp_id ?>">
-                                                    <button class="panel_button" name="Close panel" type="submit" name="submit_delete" id="update">
+                                                    <input id='plus_info_id' name='entry_id' type='text' value='<?php echo $row['id'] ?>' class='hidden' >
+                                                    <button class="panel_button" type="submit" name="submit_delete" id="update">
                                                         <img src="../assets/icons/delete_black.svg">
                                                     </button>
-                                                    <textarea id="update" name="text_body"><?php echo $info_body ?></textarea>
-                                                    <input id="update" name="text_title_to_change" type="text" value="<?php echo $info_title ?>" class="hidden">
+                                                    <textarea placeholder='Type your information here...' id="update" name="text_body"><?php echo $row['body'] ?></textarea>
+                                                    <input id="update" name="text_title_to_change" type="text" value="<?php echo $row['body'] ?>" class="hidden">
                                                     <input id="update" name="submit_body" type="submit" value="Save" class="panel_submit">
                                                 </form>
                                             </div>
                                     <?php
                                         }
-                                    }
                                     ?>
                                     <div id="add_entry" onclick="hideNshow()">
                                         <div class="table_row" onclick="">
@@ -255,7 +223,7 @@ if (file_exists("../uploads/$comp_id.png")) {
 
                 <div id="cw_preview" class="paper_wrapper">
                     <div id="title_stripe_cw">
-                        <img src="<?php echo $logo_path ?>" width="50" height="50" alt="<?php echo $comp_name ?>'s logo">
+                        <img src="<?php echo $logo ?>" width="50" height="50" alt="<?php echo $comp_name ?>'s logo">
 
                         <h1><?php echo $comp_name ?></h1>
                         <button value="<?php echo $comp_id ?>" class="bookmark_button" onclick="favButton(this)">
@@ -263,9 +231,9 @@ if (file_exists("../uploads/$comp_id.png")) {
                         </button>
                         <p id="comp_status"><?php echo statusConverter($comp_status) ?></p>
                         <div>
-                            <p><?php echo sexConverter($comp_sex) . "'s" ?></p>
-                            <p><?php echo weaponConverter($comp_weapon) ?></p>
-                            <p><?php echo $starting_date ?></p>
+                            <p><?php echo sexConverter($comp_sex) . "'s" ?></p>&#8239
+                            <p><?php echo weaponConverter($comp_weapon) ?></p>&#8239
+                            <p><?php echo $basic_info -> starting_date ?></p>&#8239
                             <p><?php echo "" ?></p>
                         </div>
                     </div>
@@ -307,30 +275,30 @@ if (file_exists("../uploads/$comp_id.png")) {
                                             <div>
                                                 <div>
                                                     <label>HOST COUNTRY</label>
-                                                    <p><?php echo $host_country ?></p>
+                                                    <p><?php echo $basic_info -> host_country ?></p>
                                                 </div>
                                                 <div>
                                                     <label>LOCATION AND ADDRESS</label>
-                                                    <p><?php echo $city_street ?></p>
-                                                    <p><?php echo $zip_code ?></p>
+                                                    <p><?php echo $basic_info -> city_street ?></p>
+                                                    <p><?php echo $basic_info -> zip_code ?></p>
                                                 </div>
                                                 <div>
                                                     <label>ENTRY-FEE</label>
-                                                    <p><?php echo $entry_fee ?></p>
+                                                    <p><?php echo $basic_info -> entry_fee ?></p>
                                                 </div>
                                             </div>
                                             <div>
                                                 <div>
                                                     <label>STARTING DATE</label>
-                                                    <p><?php echo $starting_date ?></p>
+                                                    <p><?php echo $basic_info -> starting_date ?></p>
                                                 </div>
                                                 <div>
                                                     <label>ENDING DATE</label>
-                                                    <p><?php echo $ending_date ?></p>
+                                                    <p><?php echo $basic_info -> ending_date ?></p>
                                                 </div>
                                                 <div>
                                                     <label>END OF PRE-REGISTRTATION</label>
-                                                    <p><?php echo $end_of_pre_reg ?></p>
+                                                    <p><?php echo $basic_info -> end_of_pre_reg ?></p>
                                                 </div>
                                             </div>
                                         </div>
@@ -380,8 +348,9 @@ if (file_exists("../uploads/$comp_id.png")) {
                                 <!-- additional info panel -->
                                 <div id="additional_panel" class="column_panel breakpoint">
                                     <p class="column_panel_title">Additional information for Fencers:</p>
+
                                     <div>
-                                        <p><?php echo $comp_info ?></p>
+                                        <p><?php echo "????" ?></p>
                                     </div>
                                 </div>
 
@@ -389,48 +358,48 @@ if (file_exists("../uploads/$comp_id.png")) {
                                 <div id="weapon_control_panel" class="column_panel breakpoint">
                                     <p class="column_panel_title">Weapon Control appointments and bookings:</p>
                                     <div>
+                                        <?php
+                                            $qry_get_t_id = "SELECT ass_tournament_id FROM competitions WHERE comp_id = '$comp_id'";
+                                            $do_get_t_id = mysqli_query($connection, $qry_get_t_id);
+                                            $t_id = mysqli_fetch_assoc($do_get_t_id)['ass_tournament_id'];
+                                        $qry = "SELECT appointments FROM tournaments WHERE id = '$t_id'";
+                                        $do = mysqli_query($connection, $qry);
+                                        $string = mysqli_fetch_assoc($do)['appointments'];
+                                        $table = json_decode($string);
+                                            foreach ($table as $day => $foo) {
+                                        ?>
                                         <div class="weapon_control_day">
-                                            <p>{Weapon Control Date}</p>
+                                            <p><?php echo $day ?></p>
                                         </div>
-
-                                        <div class="weapon_control_day">
-                                            <p>{Weapon Control Date}</p>
-                                        </div>
+                                        <?php } ?>
                                     </div>
                                 </div>
 
                                 <div id="plus_information_panel" class="column_panel breakpoint">
                                     <p class="column_panel_title">Plus Information:</p>
                                     <div>
-                                        <?php
 
-                                            //display plus info from DB
 
-                                            $get_plsuinfo_qry = "SELECT * FROM info_$comp_id";
-                                            if (!$get_plsuinfo_do = mysqli_query($connection, $get_plsuinfo_qry)) {
-                                                $feedback = "ERROR: " . mysqli_error($connection);
-                                            }
-
-                                            if ($get_plsuinfo_do !== FALSE) {//checks whether table exists
-                                                while ($row = mysqli_fetch_assoc($get_plsuinfo_do)) {
-
-                                                    $info_title = $row['info_title'];
-                                                    $info_body = $row['info_body'];
-                                        ?>
+                                    <?php
+                                        $empty = true;
+                                        $qry = "SELECT title, body FROM plus_info WHERE assoc_comp_id = '$comp_id'";
+                                        $do = mysqli_query($connection, $qry);
+                                        while ($row = mysqli_fetch_assoc($do)) {
+                                            $empty = false;
+                                    ?>
                                                     <div class="breakpoint">
-                                                        <p><?php echo $info_title ?></p>
-                                                        <p><?php echo $info_body ?></p>
+                                                        <p><?php echo $row['title'] ?></p>
+                                                        <p><?php echo $row['body'] ?></p>
                                                     </div>
-                                        <?php
-                                                }
-                                            } else  { // displayed when there are no plus infos for comp_id
-                                        ?>
+
+                                    <?php
+                                        }
+                                        if ($empty) {
+                                    ?>
 
                                             <p>This competition has no plus information!</p>
+                                        <?php } ?>
 
-                                        <?php
-                                            }
-                                        ?>
                                     </div>
                                 </div>
                             </div>
